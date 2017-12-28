84029b02d54612013fe934ee8fdce241e28eaf16
dbaric <drazen@dump.hr>
2017-12-28 18:53:03 +0100

    finished root setup :tada:

--- LiftOff.Mobile/App.js
+++ LiftOff.Mobile/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { styles } from './styles.js';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.statusBar}>
-        <Text>Ova aplikacija pokazuje :tuki:</Text>
+        <Text>:tuki:</Text>
       </View>
     );
   }
 }
+
+export default App;
