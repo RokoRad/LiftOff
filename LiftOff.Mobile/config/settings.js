346efd1fdfb04cbb162f9dba131afeb12229daa8
dbaric <drazen@dump.hr>
2017-12-30 21:40:33 +0100

    cleaned up language including

--- LiftOff.Mobile/config/settings.js
+++ LiftOff.Mobile/config/settings.js
@@ -2,16 +2,9 @@
 let lang = 'hr',
     language;
 
-// za naznačeni jezik vraćamo response, default je engleski
-if (lang === 'hr') {
-  import('./languages/hr.js').then((response) => { 
-    language = response.language;
-  });
-} else {
-  import('./languages/en.js').then((response) => { 
-    language = response.language;
-  });
-}
+// dinamčiko includanje jezika te primanje podataka
+import(`./languages/${lang}.js`).then((response) => language = response.language);
+
 
 // vraćanje jezika
 export { language };
