2ed6e50a2500b9face29567d7e8b274c8d4bc0cc
dbaric <drazen@dump.hr>
2017-12-30 21:48:17 +0100

    storage wip

---
+++ LiftOff.Mobile/functions/storage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AsyncStorage } from 'react-native';
+
+const storage = {
+  set: function() {
+
+  },
+  get: function() {
+
+  },
+  remove: function() {
+
+  },
+  check: function() {
+
+  }
+}
