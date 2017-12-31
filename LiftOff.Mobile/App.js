aa6809fa961e17b40f8423e8934fd51cbff46ae2
dbaric <drazen@dump.hr>
2017-12-31 11:35:57 +0100

    router wip

--- LiftOff.Mobile/App.js
+++ LiftOff.Mobile/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles.js';
 import Screen from './components/Screen';
+import { NativeRouter } from 'react-router-native';
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.statusBar}><Text>kurac</Text>
-        <Screen>eeee</Screen>
-      </View>
+      <NativeRouter>
+        <View style={styles.statusBar}>
+          <Text>kurac</Text>
+          <Screen>eeee</Screen>
+        </View>
+      </NativeRouter>
     );
   }
 }
