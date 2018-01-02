8150f9916d0e910b8498f57e3ab3ad9ece5fbd3c
dbaric <drazen@dump.hr>
2018-01-02 16:22:19 +0100

    making button look like alive

--- LiftOff.Mobile/components/LoginButton/index.js
+++ LiftOff.Mobile/components/LoginButton/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles.js';
 import { language } from '../../config/settings.js'
 
 const LoginButton = (props) => (
-  <View style={styles.loginButtonWrapper}>
-    <Text style={styles.loginButton}>
-      {
-        props.type === 'signIn'
-        ? language.signIn
-        : language.signUp
-      }
-    </Text>
-  </View>
+  <TouchableOpacity onPress={() => props.type === 'signIn' ? console.log("signIn") : console.log("signUp")}>
+    <View style={styles.loginButtonWrapper}>
+        <Text style={styles.loginButton}>
+          {
+            props.type === 'signIn'
+            ? language.signIn
+            : language.signUp
+          }
+        </Text>
+      </View>
+  </TouchableOpacity>
 );
 
 export default LoginButton;
