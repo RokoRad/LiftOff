90268d18e592d14e72ee2d596976dba9bbb7457e
Toma Puljak <toma.puljak@hotmail.com>
2018-01-03 12:19:27 +0100

    Added current weather api call and SignalR base integration

---
+++ LiftOff.Domain/Commands/GetFiveDayForecastCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftOff.Domain.Commands
+{
+    public class GetFiveDayForecastCommand
+    {
+    }
+}
