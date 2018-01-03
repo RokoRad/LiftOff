9c169ca9729a422cbece0566c0669be1d71b80c5
dbaric <drazen@dump.hr>
2018-01-03 12:05:37 +0100

    :hammer:

--- LiftOff.Mobile/components/LoginLogin/index.js
+++ LiftOff.Mobile/components/LoginLogin/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, TouchableWithoutFeedback } from 'react-native';
+import { View, TouchableWithoutFeedback, Text } from 'react-native';
 import styles from './styles.js';
 import LoginInput from '../LoginInput';
 import FacebookButton from '../FacebookButton';
 import LoginButton from '../LoginButton';
+import { language } from '../../config/settings.js';
 
 const signUp = () => (
   console.log(loginData)
@@ -19,10 +20,15 @@ const LoginLogin = () => (
     <View style={styles.inputWrapper}>
       <LoginInput icon="Email" onChangeText={(e) => loginData.email = e}/>
       <LoginInput icon="Password" onChangeText={(e) => loginData.password = e} />
+      <TouchableWithoutFeedback onPress={() => (forgotPassword())}>
+        <View>
+          <Text style={styles.forgotPassword}>{language.forgotPassword}</Text>
+        </View>
+      </TouchableWithoutFeedback>
       <FacebookButton type="login" />
     </View>
     <TouchableWithoutFeedback onPress={() => signUp()}>
-      <LoginButton type="signUp" />
+      <LoginButton type="signIn" />
     </TouchableWithoutFeedback>
   </View>
 );
