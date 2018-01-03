a50e1c59b1f221530f27401a1b462bead3d80e1b
dbaric <drazen@dump.hr>
2018-01-03 22:45:38 +0100

    finishing touches

--- LiftOff.Mobile/components/Navigation/index.js
+++ LiftOff.Mobile/components/Navigation/index.js
@@ -5,11 +5,11 @@ import styles from './styles.js';
 
 const Navigation = (props) => (
     <View style={styles.navigation}>
-      <NavigationItem icon="" route="/" type="Home" current={props.current} />
-      <NavigationItem icon="" route="/map" type="Map" current={props.current} />
-      <NavigationItem icon="" route="/liftoff" type="LiftOff" current={props.current} />
-      <NavigationItem icon="" route="/account" type="Account" current={props.current} />
-      <NavigationItem icon="" route="/settings" type="Settings" current={props.current} />
+      <NavigationItem route="/" type="Home" current={props.current} />
+      <NavigationItem route="/map" type="Map" current={props.current} />
+      <NavigationItem route="/liftoff" type="LiftOff" current={props.current} />
+      <NavigationItem route="/account" type="Account" current={props.current} />
+      <NavigationItem route="/settings" type="Settings" current={props.current} />
     </View>
 );
 
--- LiftOff.Mobile/components/NavigationItem/index.js
+++ LiftOff.Mobile/components/NavigationItem/index.js
@@ -4,13 +4,22 @@ import { Link } from 'react-router-native';
 import styles from './styles.js';
 import { language } from '../../config/settings.js';
 
+// objekt sa ppisom ruta do ikona
+const icons = {
+  Home: require('../../images/drone-nav.png'),
+  Map: require('../../images/map-nav.png'),
+  LiftOff: require('../../images/stopwatch-nav.png'),
+  Account: require('../../images/user-nav.png'),
+  Settings: require('../../images/settings-nav.png')
+}
+
 // komponenta za navItem koji dijeli rutu
 const ActiveNavItem = (props) => {
   return (
     <Link to={props.route} style={styles.navigationItemWrapper} >
       <TouchableHighlight style={styles.navigationItemActive}>
         <View>
-          <Image source={require('../../images/user-nav.png')} style={styles.navigationImage}/>
+          <Image source={icons[props.type]} style={styles.navigationImage}/>
           <Text style={styles.navigationText}>{language[props.type]}</Text>
         </View>
       </TouchableHighlight>
@@ -20,11 +29,12 @@ const ActiveNavItem = (props) => {
 
 // komponenta za navItem koje ne odgvoara ruti
 const InactiveNavItem = (props) => {
+  console.log(props.icon);
   return (
     <Link to={props.route} style={styles.navigationItemWrapper} >
       <TouchableHighlight style={styles.navigationItem}>
         <View>
-          <Image source={require('../../images/user-nav.png')} style={styles.navigationImage}/>
+          <Image source={icons[props.type]} style={styles.navigationImage}/>
           <Text style={styles.navigationText}>{language[props.type]}</Text>
         </View>
       </TouchableHighlight>
