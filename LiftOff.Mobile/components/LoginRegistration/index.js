804450afa61741d2b9df45c22a1f4f4c41448f9f
dbaric <drazen@dump.hr>
2018-01-03 10:45:41 +0100

    forgotPassword button handles clicks

--- LiftOff.Mobile/components/LoginRegistration/index.js
+++ LiftOff.Mobile/components/LoginRegistration/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import styles from './styles.js';
 import LoginInput from '../LoginInput';
 import FacebookButton from '../FacebookButton';
 import LoginButton from '../LoginButton';
 import { language } from '../../config/settings.js';
 
+const forgotPassword = () => (
+  console.log("forgotPassword")
+);
+
 const LoginRegistration = () => (
   <View>
     <View style={styles.inputWrapper}>
       <LoginInput icon="Email"/>
       <LoginInput icon="Password" />
-      <Text style={styles.forgotPassword}>{language.forgotPassword}</Text>
+      <TouchableWithoutFeedback onPress={() => (forgotPassword())}>
+        <View>
+          <Text style={styles.forgotPassword}>{language.forgotPassword}</Text>
+        </View>
+      </TouchableWithoutFeedback>
       <FacebookButton type="registration" />
     </View>
     <LoginButton type="signIn" />
