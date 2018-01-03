e354003248a7892707ded2a319396a78b4a55033
roko <rokorad@gmail.com>
2018-01-03 17:01:56 +0100

    doda praznu rutu za validaciju tokena

---
+++ LiftOff.API/Controllers/TokenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LiftOff.API.Controllers
+{
+    [RoutePrefix("api/token")]
+    public class TokenController : ApiController
+    {
+        [Authorize]
+        [Route("Validate")]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
