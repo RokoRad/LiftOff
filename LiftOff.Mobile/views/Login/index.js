a5e6a15c70c96cd47197e6b6d0006c92e0117300
dbaric <drazen@dump.hr>
2018-01-03 10:29:47 +0100

    adding comments

--- LiftOff.Mobile/views/Login/index.js
+++ LiftOff.Mobile/views/Login/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles.js';
+// dohvaćanje svih komponenti potrebnih za kreiranje viewa
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 import LoginHolder from '../../components/LoginHolder';
 import LoginSwitch from '../../components/LoginSwitch';
@@ -8,9 +9,10 @@ import LoginButton from '../../components/LoginButton';
 import LoginInput from '../../components/LoginInput';
 import LoginRegistration from '../../components/LoginRegistration';
 import FacebookButton from '../../components/FacebookButton';
-import { Route } from 'react-router-native';
-import Registration from '../../views/Register';
+import { Route } from 'react-router-native'; // dohvaćanje komponent za promjenu rute
+import Registration from '../../views/Register'; // dohvaćanje iduće rute
 
+// kreiranje viewa
 const Login = () => (
   <View style={styles.loginWrapper}>
     <LoginHolder />
--- LiftOff.Mobile/views/Register/index.js
+++ LiftOff.Mobile/views/Register/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles.js';
+// dohvaćanje svih komponenti potrebnih za kreiranje viewa
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 import LoginHolder from '../../components/LoginHolder';
 import LoginSwitch from '../../components/LoginSwitch';
@@ -8,9 +9,10 @@ import LoginButton from '../../components/LoginButton';
 import LoginInput from '../../components/LoginInput';
 import LoginLogin from '../../components/LoginLogin';
 import FacebookButton from '../../components/FacebookButton';
-import { Route } from 'react-router-native';
-import Login from '../../views/Login';
+import { Route } from 'react-router-native'; // dohvaćanje komponente za rutiranje
+import Login from '../../views/Login'; // dohvaćanje iduće rute
 
+// kreiranje viewa
 const Registration = () => (
   <View style={styles.loginWrapper}>
     <LoginHolder />
