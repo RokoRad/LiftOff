87e2b149c4c177a2d59615cb17ab65f34d3e158c
dbaric <drazen@dump.hr>
2018-01-03 23:34:47 +0100

    building <Screen />

--- LiftOff.Mobile/components/Screen/index.js
+++ LiftOff.Mobile/components/Screen/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import styles from './styles.js';
 import Navigation from '../Navigation';
 
 // view ima screen sa prikazom contenta i navigacije
 const Screen = (props) => (
     <View {...props} style={styles.screen}>
-      {/* {props.children} */}
-      <Text>screen</Text>
+      {props.children}
       <Navigation current={props.current} />
     </View>
 );
--- LiftOff.Mobile/views/Home/index.js
+++ LiftOff.Mobile/views/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { View } from 'react-native';
+ import { View, Text } from 'react-native';
 import styles from './styles.js';
 import { NativeRouter, Route } from 'react-router-native';
 import Screen from '../../components/Screen';
@@ -7,6 +7,7 @@ import Screen from '../../components/Screen';
 
 const Home = ({location}) => (
   <Screen current={location}>
+    <Text>ovo je sa homea</Text>
   </Screen>
 );
 
