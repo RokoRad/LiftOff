751768f9f22b834d99cd6882a9da07a617780e41
dbaric <drazen@dump.hr>
2018-01-03 11:08:55 +0100

    fixing loginHolder

--- LiftOff.Mobile/components/LoginHolder/index.js
+++ LiftOff.Mobile/components/LoginHolder/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import styles from './styles.js';
 
 const LoginHolder = (props) => (
   <View style={styles.loginHolder}>
-
+    <Text>kurcic</Text>
   </View>
 
 );
--- LiftOff.Mobile/components/LoginHolder/styles.js
+++ LiftOff.Mobile/components/LoginHolder/styles.js
@@ -1,7 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  loginHolder: {
+    backgroundColor: 'red',
+    position: 'absolute',
+    zIndex: 99,
+    width: '100%',
+    height: '25%',
+  }
 });
 
 export default styles;
