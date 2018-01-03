12abb8a9caa5ca17eded2a7048e1dc24a3da6273
dbaric <drazen@dump.hr>
2018-01-03 23:03:49 +0100

    wip

---
+++ LiftOff.Mobile/views/Settings/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const Settings = () => (
+  <View>
+    <Text>
+      Settings
+    </Text>
+  </View>
+);
+
+export default Settings;
---
+++ LiftOff.Mobile/views/Stopwatch/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const Stopwatch = () => (
+  <View>
+    <Text>
+      Stopwatch
+    </Text>
+  </View>
+);
+
+export default Stopwatch;
