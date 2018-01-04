f79bbf234bf0fdfe5fb9982dc19261acd5a66481
dbaric <drazen@dump.hr>
2018-01-04 19:16:43 +0100

    styled button

--- LiftOff.Mobile/views/Stopwatch/styles.js
+++ LiftOff.Mobile/views/Stopwatch/styles.js
@@ -3,6 +3,22 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   vertical: {
     backgroundColor: '#3498db'
+  },
+  starter: {
+    backgroundColor: '#3498db',
+    paddingTop: 20,
+    paddingBottom: 20,
+    width: 150,
+    borderRadius: 15,
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  starterText: {
+    color: '#fff',
+    fontSize: 40,
+    textAlign: 'center',
+    fontFamily: 'barlowSemiBold'
   }
 });
 
