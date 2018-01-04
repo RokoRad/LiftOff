060fde544b510c3294b7f910076e8efec9c7b672
dbaric <drazen@dump.hr>
2018-01-04 10:52:44 +0100

    wrapping up

--- LiftOff.Mobile/components/StopwatchInformation/styles.js
+++ LiftOff.Mobile/components/StopwatchInformation/styles.js
@@ -2,13 +2,31 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   holder: {
-
-  },
-  wrapper: {
-
+    marginTop: 20
   },
   inner: {
-    
+    textAlign: 'center',
+    color: 'grey',
+    fontSize: 16,
+    fontFamily: 'barlowMedium'
+  },
+  green: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: 'barlowMedium',
+    color: 'green'
+  },
+  orange: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: 'barlowMedium',
+    color: 'orange'
+  },
+  red: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: 'barlowMedium',
+    color: 'red'
   }
 });
 
--- LiftOff.Mobile/views/Stopwatch/index.js
+++ LiftOff.Mobile/views/Stopwatch/index.js
@@ -4,12 +4,13 @@ import styles from './styles.js';
 import Screen from '../../components/Screen';
 import SafetyscoreStopwatch from '../../components/SafetyscoreStopwatch';
 import StopwatchElement from '../../components/StopwatchElement';
+import StopwatchInformation from '../../components/StopwatchInformation';
 
 const Stopwatch = ({location}) => (
   <Screen current={location}>
   {/* <Text>rip</Text> */}
   <StopwatchElement minutes="22" seconds="33" />
-  <StopwatchInformation rating="" />
+  <StopwatchInformation battery="0.3" />
     {/* <SafetyscoreStopwatch />
     <View style={styles.stopwatchHolder}>
       <StopwatchElement />
