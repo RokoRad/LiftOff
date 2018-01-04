cd4bda2967c386a9afc02046b87a3014aa66fa7e
dbaric <drazen@dump.hr>
2018-01-04 18:10:25 +0100

    stopwatch logic

--- LiftOff.Mobile/views/Stopwatch/index.js
+++ LiftOff.Mobile/views/Stopwatch/index.js
@@ -9,60 +9,36 @@ import StopwatchInformation from '../../components/StopwatchInformation';
 
 import storage from '../../functions/storage';
 
-
-
-// var myVar = setInterval(function(){ myTimer() }, 1000);
-
-// function myTimer() {
-//     var d = new Date();
-//     var t = d.toLocaleTimeString();
-//     document.getElementById("demo").innerHTML = t;
-// }
-
-// function myStopFunction() {
-//     clearInterval(myVar);
-// }
-
-
-let seconds;
-let old;
-let started = false;
-let interval;
+let seconds = 0,
+  minutes = 0,
+  current = 0,
+  old = 0,
+  interval,
+  started = false;
 
 function bind() {
   if(started) {
-    old = seconds;
-    seconds = new Date().getTime() / 1000;
+    old = current;
+    current = new Date().getTime() / 1000;
     clearInterval(interval);
     started = false;
+    current = 0;
     seconds = 0;
+    minutes = 0;
   } else {
-    seconds = new Date().getTime() / 1000;
+    current = new Date().getTime() / 1000;
     interval = setInterval(function() {
-      console.log("a");
+      seconds++;
+      if(seconds === 61) {
+        minutes++;
+        seconds = 0;
+      }
+      console.log(seconds);
+      console.log("m" + minutes);
     }, 1000);
     started = true;
   }
 }
-
-// function bind() {
-//   if(started) {
-//     console.log(seconds);
-//     seconds = 0;
-//     startd = false;
-//   } else {
-//     started = true;
-//     setInterval(function() {
-//       seconds++;
-//     }, 1000);
-//   }
-// }
-
-// var refreshIntervalId = setInterval(fname, 10000);
-
-// /* later */
-// clearInterval(refreshIntervalId);
-
 
 const Stopwatch = ({location}) => (
   <Screen current={location} style={styles.vertical}>
