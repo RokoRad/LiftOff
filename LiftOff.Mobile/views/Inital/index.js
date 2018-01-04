e3ab27bfe08a79e2d7b932a22cd088bc4515298d
dbaric <drazen@dump.hr>
2018-01-04 22:15:56 +0100

    wip

--- LiftOff.Mobile/views/Inital/index.js
+++ LiftOff.Mobile/views/Inital/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import styles from './styles.js';
 import LoginForm from '../../components/LoginForm';
 import RegisterForm from '../../components/RegisterForm';
+import InitalButton from '../../components/InitalButton';
 
 // landing view za login i registraciju
 const Inital = (props) => (
