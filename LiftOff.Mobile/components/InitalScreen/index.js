54ff15c1d9ef2e7d81921fb900e98df1104a7744
dbaric <drazen@dump.hr>
2018-01-04 21:35:52 +0100

    finished initalScreen :tada:

--- LiftOff.Mobile/components/InitalScreen/index.js
+++ LiftOff.Mobile/components/InitalScreen/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import styles from './styles.js';
 
-// kreiranje viewa
-const InitalScreen = () => (
+// view ima screen sa prikazom contenta sa logina i registracije
+const InitalScreen = (props) => (
   <View>
-    <Text>aaa</Text>
+    <View {...props} style={styles.screen}>
+      {props.children}
+    </View>
   </View>
 );
 
