a63014b94685c93f5d4c88e1a5828c97e9951ec1
dbaric <drazen@dump.hr>
2018-01-04 20:08:04 +0100

    :hammer:

---
+++ LiftOff.Mobile/components/StopwatchLog/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const StopwatchLog = () => (
+
+);
+
+export default StopwatchLog;
---
+++ LiftOff.Mobile/components/StopwatchLogs/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const StopwatchLogs = () => (
+
+);
+
+export default StopwatchLogs;
