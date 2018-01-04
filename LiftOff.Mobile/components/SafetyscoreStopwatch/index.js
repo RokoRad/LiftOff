55c5770ec26176f1a09c61e11406cb8f0c802d48
dbaric <drazen@dump.hr>
2018-01-04 10:03:40 +0100

    setting up <Stopwatch />

---
+++ LiftOff.Mobile/components/SafetyscoreStopwatch/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const SafetyscoreStopwatch = () => (
+  <View></View>
+);
+
+export default SafetyscoreStopwatch;
--- LiftOff.Mobile/views/Stopwatch/index.js
+++ LiftOff.Mobile/views/Stopwatch/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles.js';
 import Screen from '../../components/Screen';
+import SafetyscoreStopwatch from '../../components/SafetyscoreStopwatch';
+import StopwatchElement from '../../components/StopwatchElement';
 
 const Stopwatch = ({location}) => (
   <Screen current={location}>
-    <Text>
-      Stopwatch
-    </Text>
+    <SafetyscoreStopwatch />
+    <View style={styles.stopwatchHolder}>
+      <StopwatchElement />
+      : <StopwatchElement />
+      : <StopwatchElement />
+    </View>
+    <View style={styles.paragraphHolder}>
+      <Text style={styles.paragraph}></Text>
+    </View>
+    <View style={styles.resultsHolder}>
+      <Text style={styles.results}></Text>
+    </View>
   </Screen>
 );
 
