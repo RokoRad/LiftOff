97b5a13b19a1aee837f16506c81458de23f72396
dbaric <drazen@dump.hr>
2018-01-04 16:13:01 +0100

    hidden statusbar

--- LiftOff.Mobile/components/Screen/index.js
+++ LiftOff.Mobile/components/Screen/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import styles from './styles.js';
 import Navigation from '../Navigation';
 
 // view ima screen sa prikazom contenta i navigacije
 const Screen = (props) => (
     <View {...props} style={styles.screen}>
+      <StatusBar hidden={true} />
       {props.children}
       <Navigation current={props.current} />
     </View>
--- LiftOff.Mobile/styles.js
+++ LiftOff.Mobile/styles.js
@@ -2,10 +2,10 @@ import { StyleSheet, Platform, StatusBar } from 'react-native';
 
 const styles = StyleSheet.create({
   // Statusbar hack for Android devices
-  statusBar: {
-    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
-    backgroundColor: '#3498db'
-  },
+  // statusBar: {
+  //   paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+  //   backgroundColor: '#3498db'
+  // },
   // Screen size fix for all platforms
   fullScreen: {
     minHeight: '100%',
