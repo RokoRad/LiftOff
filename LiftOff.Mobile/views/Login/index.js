768f7a7a4da11b854d7beefee6016f87083e0c5c
dbaric <drazen@dump.hr>
2018-01-04 21:25:38 +0100

    deleting junk

--- LiftOff.Mobile/views/Login/index.js
+++ LiftOff.Mobile/views/Login/index.js
@@ -3,12 +3,6 @@ import { View, Text } from 'react-native';
 import styles from './styles.js';
 // dohvaćanje svih komponenti potrebnih za kreiranje viewa
 import KeyboardSpacer from 'react-native-keyboard-spacer';
-// import LoginHolder from '../../components/LoginHolder';
-// import LoginSwitch from '../../components/LoginSwitch';
-// import LoginButton from '../../components/LoginButton';
-// import LoginInput from '../../components/LoginInput';
-// import LoginLogin from '../../components/LoginLogin';
-// import FacebookButton from '../../components/FacebookButton';
 // import { Route } from 'react-router-native'; // dohvaćanje komponent za promjenu rute
 // import Registration from '../../views/Register'; // dohvaćanje iduće rute
 
