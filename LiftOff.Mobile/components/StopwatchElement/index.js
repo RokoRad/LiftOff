b7ab19b3ef3a2d2e0ce7f042867c09e2d8e66fed
dbaric <drazen@dump.hr>
2018-01-04 10:37:09 +0100

    styling stopwatch

--- LiftOff.Mobile/components/StopwatchElement/index.js
+++ LiftOff.Mobile/components/StopwatchElement/index.js
@@ -3,9 +3,9 @@ import { View, Text } from 'react-native';
 import styles from './styles.js';
 
 const StopwatchElement = ({location}) => (
-  <View>
+  <View style={styles.wrapper}>
     <Text style={styles.inner}>22</Text>
-    :
+    <Text style={styles.double}>:</Text>
     <Text style={styles.inner}>33</Text>
   </View>
 );
--- LiftOff.Mobile/components/StopwatchElement/styles.js
+++ LiftOff.Mobile/components/StopwatchElement/styles.js
@@ -1,15 +1,30 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
   inner: {
-    backgroundColor: 'red',
+    backgroundColor: '#3498db',
     paddingTop: 20,
-    paddingLeft: 10,
+    paddingLeft: 15,
     paddingBottom: 20,
-    paddingRight: 10,
+    paddingRight: 15,
     color: '#fff',
     fontSize: 40,
-    width: 'auto'
+    borderRadius: 15,
+    width: 'auto',
+    fontFamily: 'barlowSemiBold'
+  },
+  double: {
+    fontFamily: 'barlowExtraBold',
+    color: '#3498db',
+    fontSize: 40,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 17.5
   }
 });
 
