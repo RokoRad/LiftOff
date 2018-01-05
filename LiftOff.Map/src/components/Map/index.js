6ef297d9c9a655d220da2af7c105854c6e230817
dbaric <drazen@dump.hr>
2018-01-05 14:08:10 +0100

    wip

--- LiftOff.Map/src/components/Map/index.js
+++ LiftOff.Map/src/components/Map/index.js
@@ -1,54 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import options from '../../functions/options';
 import './styles.css';
 
-// const currentLocation = () => {
-//   navigator.geolocation.getCurrentPosition(data);
-// };
-
-// const data = (response) => {
-//   const center = {
-//     lat: response['coords'].latitude,
-//     lng: response['coords'].latitude
-//   };
-//   console.log(center);
-//   return center;
-// };
-
-// window.onload = function() {
-//   currentLocation();
-// };
-
-// const Map = (props) => (
-//   <GoogleMapReact defaultCenter={center} defaultZoom={props.zoom} options={options} />
-// );
-
-// export default Map;
-
-
-class Map extends Component {
-  constructor() {
-     super();
-     this.state = {
-        center: {
-          lat: 30,
-          lng: 30
-        }
-     };
-  };
-
-  // changeView = () => {
-  //   this.setState({
-  //      login: !this.state.login 
-  //   });
-  // };
-
-  render() {
-      return (
-        <GoogleMapReact defaultCenter={this.state.center} defaultZoom={11} options={options} />
-      );
-  }
+const center = {
+  lat: 30,
+  lng: 30
 }
+
+const currentLocation = () => {
+  navigator.geolocation.getCurrentPosition(data);
+};
+
+const data = (response) => {
+  const center = {
+    lat: response['coords'].latitude,
+    lng: response['coords'].latitude
+  };
+};
+
+const Map = (props) => (
+  <GoogleMapReact defaultCenter={center} defaultZoom={11} options={options} />
+);
 
 export default Map;
