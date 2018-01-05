7cb3d34086d49793b31656f4d2af45203274ab3b
dbaric <drazen@dump.hr>
2018-01-05 14:22:40 +0100

    refactoring

--- LiftOff.Map/src/functions/options/index.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles';
-
-const options = {
-  disableDefaultUI: true,
-  styles
-};
-
-export default options;
--- LiftOff.Map/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Map from './components/Map';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Map />, document.getElementById('root'));
-registerServiceWorker();
