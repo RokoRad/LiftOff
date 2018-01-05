a988cd4c99f5aa7172f7d710785e5c68dddc50a1
dbaric <drazen@dump.hr>
2018-01-05 12:50:03 +0100

    editing map

--- LiftOff.Map/src/components/Map/index.js
+++ LiftOff.Map/src/components/Map/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import options from '../../functions/options';
 import './styles.css';
 
 const center = {lat: 59.95, lng: 30.33};
 
 const Map = (props) => (
-  <GoogleMapReact defaultCenter={center} defaultZoom={props.zoom} />
+  <GoogleMapReact defaultCenter={center} defaultZoom={props.zoom} options={options} />
 );
 
 export default Map;
