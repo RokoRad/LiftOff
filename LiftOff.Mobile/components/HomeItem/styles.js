5e0fcf0ab1925d669e13ebcdd2a562753637d5eb
dbaric <drazen@dump.hr>
2018-01-06 15:02:17 +0100

    structing classes

--- LiftOff.Mobile/components/HomeItem/styles.js
+++ LiftOff.Mobile/components/HomeItem/styles.js
@@ -1,7 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  wrapper: {
 
+  },
+  left: {
+
+  },
+  middle: {
+
+  },
+  top: {
+    
+  },
+  title: {
+
+  },
+  bottom: {
+
+  },
+  row: {
+
+  },
+  leftText: {
+
+  },
+  rightText: {
+
+  },
+  right: {
+
+  },
+  rating: {
+
+  }
 });
 
 export default styles;
