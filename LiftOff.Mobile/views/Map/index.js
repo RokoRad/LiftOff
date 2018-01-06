9eebfedbd95f093e88321b557e853d2d4d7f7f5f
dbaric <drazen@dump.hr>
2018-01-06 17:23:20 +0100

    wip

---
+++ LiftOff.Mobile/views/Map/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { WebView  } from 'react-native';
+import styles from './styles.js';
+import Screen from '../../components/Screen';
+
+const local = '../../../LiftOff.Map/index.html';
+
+const Map = ({location}) => (
+  <Screen current={location}>
+    <WebView 
+    cacheEnabled={true} 
+    javaScriptEnabled={true}
+    source={{uri: 'http://192.168.1.104:8080/'}}
+    //source={{uri: local}}
+    domStorageEnabled={true}
+    onLoadStart={console.log("started")}
+    onLoadEnd={console.log("ended")} 
+    style={styles.screen} />
+  </Screen>
+);
+
+export default Map;
---
+++ LiftOff.Mobile/views/Map/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  screen: {
+    height: '110%'
+  }
+});
+
+export default styles;
