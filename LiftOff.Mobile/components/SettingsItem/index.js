e86eee2dbc6214e73b35260e0f24c164b4103213
dbaric <drazen@dump.hr>
2018-01-06 21:25:46 +0100

    setting settings up

---
+++ LiftOff.Mobile/components/SettingsItem/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const SettingsItem = (props) => (
+  <View>
+    <Text></Text>
+  </View>
+);
+
+export default SettingsItem;
