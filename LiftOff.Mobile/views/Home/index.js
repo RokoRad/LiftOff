fe7f6b2ac54e5167142be1c7765124fdd6c5566f
dbaric <drazen@dump.hr>
2018-01-06 12:44:21 +0100

    adding home structure

--- LiftOff.Mobile/views/Home/index.js
+++ LiftOff.Mobile/views/Home/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
- import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 import styles from './styles.js';
 import { NativeRouter, Route } from 'react-router-native';
 import Screen from '../../components/Screen';
