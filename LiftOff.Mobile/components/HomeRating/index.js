6585952c41cc328190634bbd284fd536fa182172
dbaric <drazen@dump.hr>
2018-01-06 13:04:42 +0100

    setting homeratign up

--- LiftOff.Mobile/components/HomeRating/index.js
+++ LiftOff.Mobile/components/HomeRating/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import styles from './styles.js';
 
 const HomeRating = (props) => (
-  <View>
-    <Text>
-      {prop.string}
-    </Text>
+  <View style={styles.wrapper}>
+    <View style={styles.top}>
+      {/* image */}
+    </View>
+    <View style={styles.bottom}>
+      <View style={styles.left}>
+        <Text style={styles.title}></Text>
+        <Text style={styles.text}></Text>
+      </View>
+      <View style={styles.right}>
+        <Text style={styles.rating}></Text>
+      </View>
+    </View>
   </View>
 );
 
--- LiftOff.Mobile/views/Home/index.js
+++ LiftOff.Mobile/views/Home/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles.js';
-import HomeRating from '../../components/HomeList';
+import HomeRating from '../../components/HomeRating';
 import HomeList from '../../components/HomeList';
 import Screen from '../../components/Screen';
-
 
 const Home = ({location}) => (
   <Screen current={location}>
     <HomeRating string="blablablablabala" rating="6.7" />
+    <HomeList />
   </Screen>
 );
 
