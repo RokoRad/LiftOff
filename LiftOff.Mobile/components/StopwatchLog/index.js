f7074f55ec9a656c208f39f9d66faa62b1032af9
dbaric <drazen@dump.hr>
2018-01-06 12:33:40 +0100

    fixing stopwatch

--- LiftOff.Mobile/components/StopwatchLog/index.js
+++ LiftOff.Mobile/components/StopwatchLog/index.js
@@ -7,7 +7,7 @@ const StopwatchLog = () => (
     <View style={styles.left}>
     </View>
     <View style={styles.middle}>
-      <Text style={styles.middleInner}>Split, Croatia (12.2E, 45N)</Text>
+      <Text style={styles.middleInner} numberOfLines={1} ellipsizeMode="tail">Split, Croatia</Text>
     </View>
     <View style={styles.right}>
       <Text style={styles.rightInner}>12:22</Text>
