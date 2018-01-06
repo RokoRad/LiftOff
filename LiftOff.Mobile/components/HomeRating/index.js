83b2bfbd940a6aa2f0b0c98111234965c57525ec
dbaric <drazen@dump.hr>
2018-01-06 14:15:08 +0100

    fixing layout

--- LiftOff.Mobile/components/HomeRating/index.js
+++ LiftOff.Mobile/components/HomeRating/index.js
@@ -3,27 +3,33 @@ import { View, Text, Image } from 'react-native';
 import styles from './styles.js';
 import { language } from '../../config/settings.js'
 
-const HomeRating = (props) => (
-  <View style={styles.wrapper}>
-    <View style={styles.top}>
-      {/* image */}
-    </View>
-    <View style={styles.bottom}>
-      <View style={styles.left}>
-        <Text style={styles.title}>
-          {language.ratingTitle}
-        </Text>
-        <Text style={styles.text}>
-          {props.string}
-        </Text>
+const HomeRating = (props) => {
+
+
+
+
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.top}>
+        {/* image */}
       </View>
-      <View style={styles.right}>
-        <Text style={styles.rating}>
-          {props.rating}
-        </Text>
+      <View style={styles.bottom}>
+        <View style={styles.left}>
+          <Text style={styles.title}>
+            {language.ratingTitle}
+          </Text>
+          <Text style={styles.text}>
+            {props.string}
+          </Text>
+        </View>
+        <View style={styles.right}>
+          <Text style={styles.rating}>
+            {props.rating}
+          </Text>
+        </View>
       </View>
     </View>
-  </View>
-);
+  )
+};
 
 export default HomeRating;
--- LiftOff.Mobile/components/HomeRating/styles.js
+++ LiftOff.Mobile/components/HomeRating/styles.js
@@ -2,28 +2,42 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   wrapper: {
-
+    height: '50%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#47e389'
   },
   top: {
-
+    flex: 1
   },
   bottom: {
-
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    flexBasis: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 15
   },
   left: {
-
+    width: '75%',
   },
   title: {
-
+    color: '#fff',
+    fontFamily: 'barlowSemiBold'
   },
   text: {
-
+    color: '#fff',
+    fontFamily: 'barlowMedium'
   },
   right: {
-
+    width: '25%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   rating: {
-    
+    color: '#fff',
+    fontFamily: 'barlowMedium'
   }
 });
 
