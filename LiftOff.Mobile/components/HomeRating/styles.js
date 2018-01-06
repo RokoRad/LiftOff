3c8867d4e4543f2b301b876c5b4fd9dcd372aa5b
dbaric <drazen@dump.hr>
2018-01-06 13:08:52 +0100

    :hammer:

---
+++ LiftOff.Mobile/components/HomeRating/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+
+  },
+  top: {
+
+  },
+  bottom: {
+
+  },
+  left: {
+
+  },
+  title: {
+
+  },
+  text: {
+
+  },
+  right: {
+
+  },
+  rating: {
+    
+  }
+});
+
+export default styles;
