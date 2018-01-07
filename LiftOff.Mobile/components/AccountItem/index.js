ef4573749e87b0d75fa67fb6d61c0204162035db
dbaric <drazen@dump.hr>
2018-01-07 15:49:22 +0100

    :hammer:

---
+++ LiftOff.Mobile/components/AccountItem/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { language } from '../../config/settings.js';
+import styles from './styles.js';
+
+const AccountItem = (props) => (
+
+);
+
+export default AccountItem;
