fa3dc17e362c69731db3c3e9a24971e3cbf3898f
dbaric <drazen@dump.hr>
2018-01-07 18:22:01 +0100

    :hammer:

--- LiftOff.Mobile/components/AccountItem/index.js
+++ LiftOff.Mobile/components/AccountItem/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import ModalPicker from 'react-native-modal-picker';
 import { language } from '../../config/settings.js';
@@ -9,34 +9,31 @@ const Default = (props) => [
   <Text style={styles.string} key={Math.random()}>{props.content}</Text>
 ];
 
-const Pick = () => {
-  let index = 0;
-  const data = [
-      { key: index++, section: true, label: 'Fruits' },
-      { key: index++, label: 'Red Apples' },
-      { key: index++, label: 'Cherries' },
-      { key: index++, label: 'Cranberries' },
-      { key: index++, label: 'Pink Grapefruit' },
-      { key: index++, label: 'Raspberries' },
-      { key: index++, section: true, label: 'Vegetables' },
-      { key: index++, label: 'Beets' },
-      { key: index++, label: 'Red Peppers' },
-      { key: index++, label: 'Radishes' },
-      { key: index++, label: 'Radicchio' },
-      { key: index++, label: 'Red Onions' },
-      { key: index++, label: 'Red Potatoes' },
-      { key: index++, label: 'Rhubarb' },
-      { key: index++, label: 'Tomatoes' }
-  ];
-  render() {
-    return (
-      <ModalPicker
-      data={data}
-      initValue="Select something yummy!"
-      onChange={(option)=>{ alert(`${option.label} (${option.key}) nom nom nom`) }} />
-    )
-  }
-};
+let index = 0;
+const data = [
+    { key: index++, label: 'Fruits' },
+    { key: index++, label: 'Red Apples' },
+    { key: index++, label: 'Cherries' },
+    { key: index++, label: 'Cranberries' },
+    { key: index++, label: 'Pink Grapefruit' },
+    { key: index++, label: 'Raspberries' },
+    { key: index++, label: 'Vegetables' },
+    { key: index++, label: 'Beets' },
+    { key: index++, label: 'Red Peppers' },
+    { key: index++, label: 'Radishes' },
+    { key: index++, label: 'Radicchio' },
+    { key: index++, label: 'Red Onions' },
+    { key: index++, label: 'Red Potatoes' },
+    { key: index++, label: 'Rhubarb' },
+    { key: index++, label: 'Tomatoes' }
+];
+
+const Pick = () => (
+  <ModalPicker
+    data={data}
+    initValue="Select something yummy!"
+    onChange={(option) => console.log(option)} />
+);
 
 const AccountItem = (props) => (
   <View style={styles.wrapper}>
