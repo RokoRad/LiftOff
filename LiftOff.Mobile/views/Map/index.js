6594bf9523e6f4a8ab3e6ba368f2cd5e7869b389
dbaric <drazen@dump.hr>
2018-01-07 09:29:12 +0100

    wip

--- LiftOff.Mobile/views/Map/index.js
+++ LiftOff.Mobile/views/Map/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import Screen from '../../components/Screen';
 import style from './map.js'
-import { MapView } from 'expo';
+import { MapView, PROVIDER_GOOGLE } from 'expo';
 
 const Map = ({location}) => (
    <Screen current={location}>
-      <MapView
-        style={{ flex: 1 }},
-        customMapStyle={}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <MapView style={{ flex: 1 }} provider={PROVIDER_GOOGLE} customMapStyle={style} initialRegion={{ latitude: 37.78825, longitude: -122.4324 }} />
   </Screen>
 );
 
