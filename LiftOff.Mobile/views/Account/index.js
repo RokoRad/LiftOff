5cc7aab3aa8d1e7e4d11665dd96919be36db8ed7
dbaric <drazen@dump.hr>
2018-01-07 16:57:55 +0100

    :hammer:

--- LiftOff.Mobile/views/Account/index.js
+++ LiftOff.Mobile/views/Account/index.js
@@ -7,7 +7,7 @@ import styles from './styles.js';
 
 const Account = ({location}) => (
   <Screen current={location}>
-    <AccountMap latitude="45" longitude="44" />
+    <AccountMap latitude={45} longitude={30} />
     <AccountItem title="Username" content="dbaric"/>
     <AccountItem title="Username" content="dbaric"/>
     <AccountItem title="Username" content="dbaric"/>
