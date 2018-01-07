3604e5188ae5a16509f6fb41b24f9e06bef43770
dbaric <drazen@dump.hr>
2018-01-07 10:47:50 +0100

    wip

---
+++ LiftOff.Mobile/components/MapItem/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import styles from './styles.js';
+
+const MapItem = (props) => ();
+
+export default MapItem;
