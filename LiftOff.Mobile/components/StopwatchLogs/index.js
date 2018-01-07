418faca6d0723c8845f89aa68456c2f1321cde3e
dbaric <drazen@dump.hr>
2018-01-07 20:10:26 +0100

    :hammer:

--- LiftOff.Mobile/components/StopwatchLogs/index.js
+++ LiftOff.Mobile/components/StopwatchLogs/index.js
@@ -3,6 +3,12 @@ import { View, Text, ScrollView } from 'react-native';
 import styles from './styles.js';
 import StopwatchLog from '../StopwatchLog';
 
+const data = [
+  {id: 1, active: true, location: 'Čavoglave, Croatia', time: '22:10'},
+  {id: 2, active: false, location: 'Čavoglave, Croatia', time: '12:10'},
+  {id: 3, active: false, location: 'Čavoglave, Croatia', time: '07:10'}
+];
+
 const StopwatchLogs = () => (
   <View style={styles.wrapper}>
     <View style={styles.head}>
@@ -11,16 +17,18 @@ const StopwatchLogs = () => (
       <Text style={styles.right}>Time</Text>
     </View>
     <ScrollView style={styles.scroll}>
-      <StopwatchLog active />
-      <StopwatchLog />
-      <StopwatchLog active />
-      <StopwatchLog />
-      <StopwatchLog />
-      <StopwatchLog active />
-      <StopwatchLog />
-      <StopwatchLog />
+      {data.map((value) => <StopwatchLog location={value.location} time={value.time} active={value.active} key={value.id} />)}
     </ScrollView>
   </View>
 );
 
 export default StopwatchLogs;
+
+{/* <StopwatchLog active />
+<StopwatchLog />
+<StopwatchLog active />
+<StopwatchLog />
+<StopwatchLog />
+<StopwatchLog active />
+<StopwatchLog />
+<StopwatchLog /> */}
