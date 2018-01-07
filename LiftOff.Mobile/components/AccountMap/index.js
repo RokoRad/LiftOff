94c77c9b86af88ccc3d8d9ebfbf3765b357e26b1
dbaric <drazen@dump.hr>
2018-01-07 22:39:17 +0100

    :hammer:

--- LiftOff.Mobile/components/AccountMap/index.js
+++ LiftOff.Mobile/components/AccountMap/index.js
@@ -7,7 +7,7 @@ import styles from './styles.js';
 const AccountMap = (props) => (
   <View style={styles.wrapper}>
     <Text style={styles.text}>
-      More than 30 flew here <Image source={require('../../images/map/fire.png')} style={styles.image}/>
+      More than {props.number} flew here <Image source={require('../../images/map/fire.png')} style={styles.image}/>
       </Text>
     <MapView zoomEnabled={false} style={{ flex: 1 }} provider={PROVIDER_GOOGLE} customMapStyle={style} pitchEnabled={false} cacheEnabled={true} scrollEnabled={false}
       region={{ latitude: props.latitude, longitude: props.longitude, latitudeDelta: 0.1, longitudeDelta: 0.05 }} rotateEnabled={false}>
--- LiftOff.Mobile/views/Account/index.js
+++ LiftOff.Mobile/views/Account/index.js
@@ -7,12 +7,16 @@ import Dropdown from '../../components/Dropdown';
 import styles from './styles.js';
 
 const data = [
-  {location: {latitude: '', latitudeDelta: '', longitude: '', longitudeDelta: ''}}
+  {location: {latitude: 45.815399, latitudeDelta: 0.1, longitude: 15.866568, longitudeDelta: 0.05}},
+  {location: {latitude: 45.915399, latitudeDelta: 0.1, longitude: 15.766568, longitudeDelta: 0.05}},
+  {location: {latitude: 45.715399, latitudeDelta: 0.1, longitude: 15.666568, longitudeDelta: 0.05}},
+  {location: {latitude: 45.615399, latitudeDelta: 0.1, longitude: 15.566568, longitudeDelta: 0.05}},
+  {location: {latitude: 45.515399, latitudeDelta: 0.1, longitude: 15.466568, longitudeDelta: 0.05}}
 ];
 
 const Account = ({location}) => (
   <Screen current={location}>
-    <AccountMap latitude={45.815399} longitude={15.966568} data="" />
+    <AccountMap latitude={45.815399} longitude={15.966568} data={data} number={data.length} />
     <View style={styles.container}>
       <AccountItem title="Username" content="dbaric"/>
       <AccountItem title="Email" content="dbaric@dump.hr"/>
