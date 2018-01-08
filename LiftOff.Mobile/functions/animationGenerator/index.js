83eed45cf6f43aa84436f9e345e228c72438dfff
dbaric <drazen@dump.hr>
2018-01-08 22:58:06 +0100

    :nail_care:

--- LiftOff.Mobile/functions/animationGenerator/index.js
+++ LiftOff.Mobile/functions/animationGenerator/index.js
@@ -3,26 +3,32 @@ import * as Animatable from 'react-native-animatable';
 Animatable.initializeRegistryWithDefinitions({
   green: {
     from: {
-      rotate: '-1deg'
+      rotate: '-1deg',
+      translateX: 3
     },
     to: {
-      rotate: '1deg'
+      rotate: '1deg',
+      translateX: -3
     }    
   },
   orange: {
     from: {
-      rotate: '-2deg'
+      rotate: '-2deg',
+      translateX: 5
     },
     to: {
-      rotate: '2deg'
+      rotate: '2deg',
+      translateX: -5
     }    
   },
   red: {
     from: {
-      rotate: '-3deg'
+      rotate: '-3deg',
+      translateX: 8
     },
     to: {
-      rotate: '3deg'
+      rotate: '3deg',
+      translateX: -8
     }    
   }
 });
