43c2540bcfaee23dc5ae103f81c8c2735f35c06b
Tpuljak <toma.puljak@hotmail.com>
2018-01-08 16:57:09 +0100

    Files for drazen finished

--- RealTimeDrazen.js
+++ RealTimeDrazen.js
@@ -1,24 +1,14 @@
 const connection = signalr.hubConnection('http://liftoffapi.azurewebsites.net/signalr');
 connection.logging = true;
 
-const proxy = connection.createHubProxy('chatHub');
-//receives broadcast messages from a hub function, called "helloApp"
-proxy.on('helloApp', (argOne, argTwo, argThree, argFour) => {
-    console.log('message-from-server', argOne, argTwo, argThree, argFour);
-    //Here I could response by calling something else on the server...
+const proxy = connection.createHubProxy('weatherHub');
+
+proxy.on('broadcastWeather', (weatherRating) => {
+    //radi sta triba sa weatherRatingon
 });
 
-// atempt connection, and handle errors
 connection.start().done(() => {
-    console.log('Now connected, connection ID=' + connection.id);
-
-    proxy.invoke('helloServer', 'Hello Server, how are you?')
-    .done((directResponse) => {
-        console.log('direct-response-from-server', directResponse);
+        proxy.invoke('InitiateConnection', timeLocation, units);
     }).fail(() => {
-        console.warn('Something went wrong when calling server, it might not be up and running?')
-    });
-
-}).fail(() => {
-    console.log('Failed');
-});
+        console.log('Failed');
+}   );
