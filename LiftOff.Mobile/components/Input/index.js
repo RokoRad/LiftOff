4475d16a6ed7718e53fe0ce041c58c2717b12969
dbaric <drazen@dump.hr>
2018-01-09 13:26:23 +0100

    :nail_care:

--- LiftOff.Mobile/components/Input/index.js
+++ LiftOff.Mobile/components/Input/index.js
@@ -3,9 +3,13 @@ import { View, TextInput, Image } from 'react-native';
 import styles from './styles.js';
 import values from '../../functions/values';
 
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 const Input = (props) => (
   <View style={styles.wrapper}>
-    <TextInput underlineColorAndroid={'transparent'} placeholder={props.type.toUpperCase()} style={styles.input} placeholderTextColor="#fff" secureTextEntry={props.type === 'password' ? true : false} onChangeText={(value) => (values[props.type] = value)} />
+    <TextInput underlineColorAndroid={'transparent'} placeholder={capitalize(props.type)} style={styles.input} placeholderTextColor="#fff" secureTextEntry={props.type === 'password' ? true : false} onChangeText={(value) => (values[props.type] = value)} />
   </View>
 );
 
