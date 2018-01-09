da7d4aae71df54523704724a7d8787ea1ebeeca3
dbaric <drazen@dump.hr>
2018-01-09 21:44:13 +0100

    :nail_care:

--- LiftOff.Mobile/views/Home/index.js
+++ LiftOff.Mobile/views/Home/index.js
@@ -4,7 +4,8 @@ import signalr from 'react-native-signalr';
 import HomeRating from '../../components/HomeRating';
 import HomeList from '../../components/HomeList';
 import Screen from '../../components/Screen';
-import proxy from '../../functions/realtime'
+import proxy from '../../functions/realtime';
+import defaultList from '../../config/defaultList.js';
 
 // const connection = signalr.hubConnection('http://liftoffapi.azurewebsites.net/');
 // connection.logging = true;
@@ -35,38 +36,7 @@ class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-      list: {
-        AtmosphereRating: null,
-        ConditionsRating: null,
-        TemperatureRating: null,
-        TotalRating: null,
-        UVRating: null,
-        VisibilityRating: null,
-        WindRating: null,
-        weatherData: {
-          Cloudiness: null,
-          Humidity: null,
-          Max_Temperature: 15.03,
-          Min_Temperature: 15.03,
-          Presssure: 1004.53,
-          Temperature: 15.03,
-          TimeLocation: {
-            Location: {
-              Latitude: 23,
-              Longitude: 33,
-            },
-            Time: "2018-01-09T19:26:18.4557311+00:00",
-          },
-          UVIndex: 5.5,
-          Units: "metric",
-          Visibility: null,
-          Weather: "Clear",
-          WeatherDescription: "clear sky",
-          WeatherID: 800,
-          WindDirection: 44.0012,
-          WindSpeed: 4.12,
-        },
-      }
+      list: defaultList
     }
   };
 
