6e8bff343d80b724c9b3575a57da7a031af99d4f
dbaric <drazen@dump.hr>
2018-01-11 00:15:18 +0100

    :hammer:

--- LiftOff.Web/src/index.js
+++ LiftOff.Web/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './views/Login';
+import Dashboard from './views/Dashboard';
+import './global.css';
 import './index.css';
+
 // APP JS
 import registerServiceWorker from './registerServiceWorker';
 
