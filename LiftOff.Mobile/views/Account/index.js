0d46db87100db71c385c144c261082f13ac96959
dbaric <drazen@dump.hr>
2018-01-10 22:30:54 +0100

    :hammer:

--- LiftOff.Mobile/views/Account/index.js
+++ LiftOff.Mobile/views/Account/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Picker, ScrollView, AsyncStorage } from 'react-native';
 import Screen from '../../components/Screen';
 import AccountItem from '../../components/AccountItem';
@@ -8,27 +8,77 @@ import styles from './styles.js';
 
 // "_bodyInit": "{\"id\":\"14114859-8b49-4bf9-bd9a-78969cb085ea\",\"userName\":null,\"totalTimeFlown\":828,\"totalFlights\":12,\"totalFlySafeScore\":26.399999999999995,\"favoriteFlightTime\":\"Afternoon\",\"favoriteFlightSpot\":\"Split\",\"showWhereIFly\":true}",
 
+// const userObject = AsyncStorage.getItem('@token').then((value) => {
+//   fetch('http://liftoffapi.azurewebsites.net/api/account/getUserData', {
+//     method: 'GET',
+//     headers: {
+//       'Authorization': 'Bearer ' + value,
+//       'Content-type': 'application/json'
+//     }
+//   }).then((response) => console.log(response._bodyInit));
+// });
 
-const stats = AsyncStorage.getItem('@stats').then((value) => JSON.parse(value));
 
-const Account = ({location}) => (
-  <Screen current={location}>
-  {console.log(stats)}
-    <AccountMap latitude={45.80} longitude={15.95} />
-    <View style={styles.container}>
-      <AccountItem title="Username" content="dbaric"/>
-      <AccountItem title="Email" content="dbaric@dump.hr"/>
-      <Dropdown />
-      <ScrollView style={styles.scrollview}>
-        <AccountItem title="Total flights" content="18"/>
-        <AccountItem title="Total flights" content="18"/>
-        <AccountItem title="Total flights" content="18"/>
-        <AccountItem title="Total flights" content="18"/>
-        <AccountItem title="Total flights" content="18"/>
-        <AccountItem title="Total flights" content="18"/>
-      </ScrollView>
-    </View>
-  </Screen>
-);
+class Account extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userObject: {
+        username: 'a'
+      }
+    }
+  };
+
+  componentWillMount() {
+    this.setState({userObject: AsyncStorage.getItem('@token').then((value) => {
+      fetch('http://liftoffapi.azurewebsites.net/api/account/getUserData', {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + value
+        }
+      }).then((response) => JSON.parse(response._bodyInit));
+    })
+  })}
+
+  render() {
+    return (
+      <Screen current={this.props.location}>
+        <AccountMap latitude={45.80} longitude={15.95} />
+        <View style={styles.container}>
+          <AccountItem title="Username" content={this.state.userObject.username}/>
+          <AccountItem title="Email" content="dbaric@dump.hr"/>
+          <Dropdown />
+          <ScrollView style={styles.scrollview}>
+            <AccountItem title="Total flights" content="18"/>
+            <AccountItem title="Total flights" content="18"/>
+            <AccountItem title="Total flights" content="18"/>
+            <AccountItem title="Total flights" content="18"/>
+            <AccountItem title="Total flights" content="18"/>
+            <AccountItem title="Total flights" content="18"/>
+          </ScrollView>
+        </View>
+      </Screen>
+    );
+  }
+}
+
+// const Account = ({location}) => (
+//   <Screen current={location}>
+//     <AccountMap latitude={45.80} longitude={15.95} />
+//     <View style={styles.container}>
+//       <AccountItem title="Username" content={userObject.username}/>
+//       <AccountItem title="Email" content="dbaric@dump.hr"/>
+//       <Dropdown />
+//       <ScrollView style={styles.scrollview}>
+//         <AccountItem title="Total flights" content="18"/>
+//         <AccountItem title="Total flights" content="18"/>
+//         <AccountItem title="Total flights" content="18"/>
+//         <AccountItem title="Total flights" content="18"/>
+//         <AccountItem title="Total flights" content="18"/>
+//         <AccountItem title="Total flights" content="18"/>
+//       </ScrollView>
+//     </View>
+//   </Screen>
+// );
 
 export default Account;
