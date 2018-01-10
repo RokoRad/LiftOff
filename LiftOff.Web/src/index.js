5d1d77686a4f6988808ffe2ddd42a997a5b991b6
dbaric <drazen@dump.hr>
2018-01-11 00:24:35 +0100

    :hammer:

--- LiftOff.Web/src/index.js
+++ LiftOff.Web/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 import Login from './views/Login';
 import Dashboard from './views/Dashboard';
 import './global.css';
 import './index.css';
 
-// APP JS
-import registerServiceWorker from './registerServiceWorker';
+const App = () => (
+  <BrowserRouter>
+    <Route exact strict path="/" component={Login}/>
+    <Route exact strict path="/dashboard" component={Dashboard}/>
+  </BrowserRouter>
+);
 
-ReactDOM.render(/* APPJS */, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
