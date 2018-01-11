15039f9e951cca3376f1aeba68a0734a22c1f574
dbaric <drazen@dump.hr>
2018-01-11 16:11:36 +0100

    :hammer:

--- LiftOff.Web/src/components/Input/index.js
+++ LiftOff.Web/src/components/Input/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 const Input = (props) => (
-  <input className="input" {...props}/>
+  <input className="input horizontal-align" {...props}/>
 );
 
 export default Input;
--- LiftOff.Web/src/components/Login/index.js
+++ LiftOff.Web/src/components/Login/index.js
@@ -6,6 +6,7 @@ import './style.css';
 const Login = () => (
   <div className="login">
     <Input placeholder="Username" />
+    <Input placeholder="Password" />
     <Button href="#">Login</Button>
   </div>
 );
