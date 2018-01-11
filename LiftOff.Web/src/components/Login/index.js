e73ef734f72a9b522456e3095add288456093b8a
dbaric <drazen@dump.hr>
2018-01-11 18:02:50 +0100

    :hammer:

--- LiftOff.Web/src/components/Login/index.js
+++ LiftOff.Web/src/components/Login/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import axois from 'axois';
 import Input from '../Input';
 import Button from '../Button';
 import InitalMessage from '../InitalMessage';
+import storage from '../../functions/storage';
 import './style.css';
+
+let holder = {
+  username: null,
+  password: null
+};
+
+const login = () => {
+
+};
 
 const Login = () => (
   <form className="login">
-    <Input placeholder="Username" />
-    <Input placeholder="Password" type="password" minLength="8" />
+    <Input placeholder="Username" onChange={() => holder.username} />
+    <Input placeholder="Password" type="password" minLength="8" onChange={() => holder.password} />
     <InitalMessage type="login" />
-    <Button href="#">Login</Button>
+    <Button onClick={() => login()}>Login</Button>
   </form>
 );
 
--- LiftOff.Web/src/components/Register/index.js
+++ LiftOff.Web/src/components/Register/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import axois from 'axois';
 import Input from '../Input';
 import Button from '../Button';
 import InitalMessage from '../InitalMessage';
 import './style.css';
 
+let holder = {
+  username: null,
+  email: null,
+  password: null
+};
+
+const register = () => {
+  axios({
+    method: 'POST',
+    url: 'http://liftoffapi.azurewebsites.net/api/account/register',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: holder
+  }).then((response) => {
+    console.log(response)
+  });
+};
+
 const Login = () => (
   <form className="login">
-    <Input placeholder="Username" />
-    <Input placeholder="Email" type="email" />
-    <Input placeholder="Password" type="password" minLength="8" />
+    <Input placeholder="Username" onChange={() => holder.username} />
+    <Input placeholder="Email" type="email" onChange={() => holder.email} />
+    <Input placeholder="Password" type="password" minLength="8" onChange={() => holder.password} />
     <InitalMessage type="register" />
-    <Button href="#">Login</Button>
+    <Button onClick={() => register()}>Login</Button>
   </form>
 );
 
