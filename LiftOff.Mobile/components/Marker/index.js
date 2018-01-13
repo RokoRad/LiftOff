7f9e1f0b1c9679fb3e29fb7742885a0be14d4923
dbaric <drazen@dump.hr>
2018-01-13 10:08:20 +0100

    :nail_care:

--- LiftOff.Mobile/components/Marker/index.js
+++ LiftOff.Mobile/components/Marker/index.js
@@ -4,17 +4,10 @@ import { MapView } from 'expo';
 import Callout from '../Callout';
 import styles from './styles.js';
 
-const Marker = ({display, location, calibration, city, time, rating}) => {
-  console.log(calibration)
-  if(display) {
-    return (
-      <MapView.Marker image={require('../../images/map/pin.png')} style={styles.marker} coordinate={location}>
-        <Callout location={city} time={time} rating={rating} shouldShow={calibration} /> 
-      </MapView.Marker> 
-    )
-  } else {
-    return (null);
-  }
-};
+const Marker = ({display, location, calibration, city, time, rating}) => (
+  <MapView.Marker image={require('../../images/map/pin.png')} style={styles.marker} coordinate={location}>
+    <Callout location={city} time={time} rating={rating} /> 
+  </MapView.Marker> 
+);
 
 export default Marker;
