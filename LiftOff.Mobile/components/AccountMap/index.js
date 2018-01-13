ba1619886ba64cdb7fb7ebc81ac8592db23a532f
dbaric <drazen@dump.hr>
2018-01-13 14:24:55 +0100

    :nail_care:

--- LiftOff.Mobile/components/AccountMap/index.js
+++ LiftOff.Mobile/components/AccountMap/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text, Image, AsyncStorage } from 'react-native';
+import LocalizedStrings from 'react-native-localization';
 import { MapView, PROVIDER_GOOGLE } from 'expo';
 import style from '../../functions/mapStyle';
 import styles from './styles.js';
 
+// instnciranje lokalizacije
+let strings = new LocalizedStrings({
+  en: {
+    moreThan: "More than",
+    flewhere: "flew here"
+  },
+  hr: {
+    moreThan: "Više od",
+    flewhere: "je letjelo ovdije"
+  }
+ });
+
+// instanciranje kompinente
 class AccountMap extends React.Component {
+  // postavljanje defaultne vrijednosti, koja je u ovom slučaju Split
   constructor() {
     super();
     this.state = {
@@ -20,11 +35,14 @@ class AccountMap extends React.Component {
     }
   }
 
+  // prilikom kreiranja komponente poziva funkciju za dohvaćanje markera
   componentWillMount() {
     this.grabMarkers();
   }
 
+  // funckija za dohvaćanje markera  
   grabMarkers = () => {
+    // postavljanje vijrednosti poziva ovisno o lokaciji
     const holder = {
       location: {
         latitude: 43.508133,
@@ -33,6 +51,7 @@ class AccountMap extends React.Component {
       time: new Date()
     };
 
+    // dohvaćanje cacha te njegovo prikazivanje
     AsyncStorage.getItem('@hot').then((value) => {
       if(JSON.parse(value).length !== 0) {
         this.setState({
@@ -41,6 +60,7 @@ class AccountMap extends React.Component {
       }
     });
 
+    // dohvaćanje podataka sa servera
     AsyncStorage.getItem('@token').then((value) => {
       fetch('http://liftoffapi.azurewebsites.net/api/flights/getFlightsNearMe', {
         method: 'POST',
@@ -50,25 +70,29 @@ class AccountMap extends React.Component {
         },
         body: JSON.stringify(holder)
       }).then((response) => {
-        AsyncStorage.setItem('@hot', response._bodyInit);
-        this.setState({
-          markers: JSON.parse(response._bodyInit)
-        });
+        if(response.status === 200) {
+          // postavljanje novog cacha te ažuiriranje podatka
+          AsyncStorage.setItem('@hot', response._bodyInit);
+          this.setState({
+            markers: JSON.parse(response._bodyInit)
+          });
+        } else if (response.status === 401) {
+          this.props.history.push('/');
+        }
       });
     })
   };
 
+  // renderiranje komponente te mapiranje markera na mapu
   render() {
     return (
       <View style={styles.wrapper}>
         <Text style={styles.text}>
-          More than {this.state.markers.length} flew here <Image source={require('../../images/map/fire.png')} style={styles.image} />
+          {strings.moreThan} {this.state.markers.length} {strings.flewHere} <Image source={require('../../images/map/fire.png')} style={styles.image} />
         </Text>
         <MapView zoomEnabled={true} style={{ flex: 1 }} provider={PROVIDER_GOOGLE} customMapStyle={style} cacheEnabled={true}
           region={{ ...this.state.markers[0].flightLocation, latitudeDelta: 0.1, longitudeDelta: 0.05 }}>
           {[this.state.markers].map((marker, index) => (
-            //console.log(marker[index].flightLocation)
-            //console.log(marker[id])
             <MapView.Marker coordinate={{ ...marker[index].flightLocation }} key={marker[index].id} image={require('../../images/map/pin.png')}/>
           ))}
         </MapView>
