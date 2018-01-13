1d256213430e96fb0f280a9fbdfa157051a0f146
dbaric <drazen@dump.hr>
2018-01-13 14:16:46 +0100

    :nail_care:

--- LiftOff.Mobile/views/Account/index.js
+++ LiftOff.Mobile/views/Account/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Picker, ScrollView, AsyncStorage } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import Screen from '../../components/Screen';
 import AccountItem from '../../components/AccountItem';
 import AccountMap from '../../components/AccountMap';
 import styles from './styles.js';
 
+// instaciranje komponente
 class Account extends Component {
+  // postavljanje defaultnih vrijednosti
   constructor() {
     super();
     this.state = {
@@ -21,13 +23,16 @@ class Account extends Component {
     }
   };
 
+  // poziv prilikom renderiranja komponente
   componentWillMount() {
+    // povlačenje vrijednosti iz cachea
     AsyncStorage.getItem('@stats').then((value) => {
       this.state = {
         userObject: JSON.parse(value)
       };
     });
 
+    // dohvaćanje tokena i postavljanje vrijednosti dobivene sa servera
     AsyncStorage.getItem('@token').then((value) => {
       fetch('http://liftoffapi.azurewebsites.net/api/account/getUserData', {
         method: 'GET',
@@ -35,18 +40,22 @@ class Account extends Component {
           'Authorization': 'Bearer ' + value
         }
       }).then((response) => {
+        // provjera povratnog statusa servera
         if(response.status === 200) {
+          // updateanje vrijednosti i cachranje
           this.setState({
             userObject: JSON.parse(response._bodyInit)
           });
           AsyncStorage.setItem('@stats', (response._bodyInit));
         } else if (response.status === 401) {
+          // za invalidan token, povratak na login
           this.props.history.push('/');
         }
       })
     })
   }
   
+  // funckija renderiranja te sadržaj komponente
   render() {
     return (
       <Screen current={this.props.location}>
