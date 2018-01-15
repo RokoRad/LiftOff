1e87087822bf65fa9b47c8c4026fd26cf69583d4
Tpuljak <toma.puljak@hotmail.com>
2018-01-15 19:49:50 +0100

    Minor

--- LiftOff.Smartwatch/StopwatchTestApp/StopwatchTestApp WatchKit Extension/InterfaceController.swift
+++ LiftOff.Smartwatch/StopwatchTestApp/StopwatchTestApp WatchKit Extension/InterfaceController.swift
@@ -8,30 +8,46 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
+public class InterfaceController: WKInterfaceController {
 
-class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
+    public override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
     
-    override func willActivate() {
+    public override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
-        
         super.willActivate()
+        
+        if WCSession.isSupported() {
+            let session = WCSession.default()
+            session.delegate = self as? WCSessionDelegate
+            session.activate()
+        }
+        
+        //PopulateTable()
     }
     
-    override func didDeactivate() {
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        let recivedScores = applicationContext["scores"] as? [NSObject]
+        scores = recivedScores!
+        FlySafeButton.setTitle(scores[0]?.value(forKey: "category") as? String)
+    }
+    
+    public override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
     
+    override public func contextForSegue(withIdentifier segueIdentifier: String) -> Any? {
+        return scores
+    }
+    
     @IBOutlet var LiftOffButton: WKInterfaceButton!
     @IBOutlet var Stopwatch: WKInterfaceTimer!
-
+    @IBOutlet var FlySafeButton: WKInterfaceButton!
+    
     @IBAction func LiftOffAction() {
         if isCounting{
             isCounting = false
@@ -65,4 +81,37 @@ class InterfaceController: WKInterfaceController {
     var myTimer : Timer?
     
     var isCounting = false
+    var scores = [NSObject?]()
+    
+//    func PopulateTable() {
+//        let token = "VJbGi_icRlxW_EIOsd0pMMT3A_hlvnJU9y8dd6PTFkEa9jXJ3K38TIXjhcq_CwiCBAN60nRHsr4swUr7GDQGKyVvMMdwf814DJoFSRRpl_o1LH32uYJcFVUuf2-6Ify582ewnU_xoyEEpa6YKPSHyXkmd9YqTvV9wvvBuPTdWe_Ztpx7NJ6j4UgC_Beu5kAtGDQyfC8LmMScJDoLsy61wyEcuE0oYDzyEBRKVbQ4nchzfJGiZm31vnpvM_sCga26zA6kHY78r9Ex2QcgOGtD9vpXQ0_In2J5kBOMGKC0DqJ5CuPux6wEhojTFgrP0JECaCeS88mf0bYsLTOkBvrFa3M27962w9bIPMMvR9_mAWs"
+//        
+//        var request: URLRequest =  URLRequest(url: URL(string: "http://liftoffapi.azurewebsites.net/api/drones/getDrones")!)
+//        request.httpMethod = "GET"
+//        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+//        
+//        URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
+//            if(error != nil) {
+//                print("Response error")
+//            }
+//            else {
+//                if let content = data {
+//                    do {
+//                        let myJSON = try JSONSerialization.jsonObject(with: content, options: .mutableContainers) as! [NSObject]
+//                        
+//                        if(myJSON[0].value(forKey: "message") == nil) {
+//                            self.scores = myJSON
+//                            self.FlySafeButton.setEnabled(true)
+//                        }
+//                        else {
+//                            print(myJSON[0].value(forKey: "message") ?? "no message")
+//                        }
+//                    }
+//                    catch {
+//                        print("Error parsing JSON")
+//                    }
+//                }
+//            }
+//        }).resume()
+//    }
 }
--- LiftOff.Smartwatch/StopwatchTestApp/StopwatchTestApp WatchKit Extension/TableInterfaceController.swift
+++ LiftOff.Smartwatch/StopwatchTestApp/StopwatchTestApp WatchKit Extension/TableInterfaceController.swift
@@ -10,6 +10,7 @@ import WatchKit
 import Foundation
 import UIKit
 
+
 struct Drone {
     let id: Int
     let name: String
@@ -23,14 +24,26 @@ class TableInterfaceController: WKInterfaceController {
         super.awake(withContext: context)
         
         // Configure interface objects here.
-        
+        if let newContext = context as? [NSObject] {
+            if (newContext.count != 0) {
+                LoadingLabel.setHidden(true)
+                self.Table.setNumberOfRows(newContext.count, withRowType: "TotalScoreRow")
+                for i in 0...newContext.count - 1 {
+                    let row = Table.rowController(at: i) as! TableRow
+                    row.FlySafeLabel.setText(newContext[i].value(forKey: "category") as? String ?? "no category")
+                    row.ScoreLabel.setText(newContext[i].value(forKey: "score") as? String ?? "/")
+                }
+            }
+        }
     }
+    
+    func reloadRootControllers(withNames string: [String], contexts: [Any]?) {}
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         
         super.willActivate()
-        PopulateTable()
+        //PopulateTable()
     }
     
     override func didDeactivate() {
@@ -42,43 +55,4 @@ class TableInterfaceController: WKInterfaceController {
     @IBOutlet var Table: WKInterfaceTable!
     
     @IBOutlet var LoadingLabel: WKInterfaceLabel!
-    
-    var items = ["1", "2", "3", "4"]
-    
-    func PopulateTable() {
-        
-        let token = "tdgYNr78WCjfWXDLPmfSdGyQQenDIhykl1FU8Cx_bIsmmchrldqP_VvjSrgkp_zWvVmtejz3uq36XUBuF7HteFbQtBOc8X3NvxrKvyJoNRxNV1I_W8sQe62dPPLIzBOplsLxpT4JNZA7fXYQvz9SCdeXaIIB5JbGE_P2QxTx6cr0QuOjWGyk4n_yUGqxMPcaO4Re84X3yt8YQ5-_ksg-GCiuvWaFWF_eRxATDInrW6dJgGIOmQv7cTXVtA4jz4HOFJ_W0YcZTUdFQMqFwKZw-nFr2RhwZtuRyLVb9mtVzEJu93SdB3xtxlg-htm_cOb3lrFjzTHR570kQfXRN8whuGrWItaaFisiTXMnMR-YoQo"
-        
-        var request: URLRequest =  URLRequest(url: URL(string: "http://liftoffapi.azurewebsites.net/api/drones/getDrones")!)
-        request.httpMethod = "GET"
-        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-
-        URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
-            if(error != nil) {
-                print("Response error")
-            }
-            else {
-                if let content = data {
-                    do {
-                        let myJSON = try JSONSerialization.jsonObject(with: content, options: .mutableContainers) as! [NSObject]
-                        
-                        self.LoadingLabel.setHidden(true)
-                        
-                        print(myJSON[0].value(forKey: "name") ?? "no value")
-                        
-                        self.Table.setNumberOfRows(myJSON.count, withRowType: "TotalScoreRow")
-                        
-                        for i in 0...myJSON.count-1 {
-                            let row = self.Table.rowController(at: i) as! TableRow
-                            row.FlySafeLabel.setText(myJSON[i].value(forKey: "name") as? String ?? "no name")
-                            row.ScoreLabel.setText(String((myJSON[i].value(forKey: "topSpeed") as? Int)!))
-                        }
-                    }
-                    catch {
-                        print("Error parsing JSON")
-                    }
-                }
-            }
-        }).resume()
-    }
 }
