698a9830a0d2ffce12e985cd0031e9d042d4a726
dbaric <drazen@dump.hr>
2018-01-16 18:06:09 +0100

    :hammer:

---
+++ LiftOff.Mobile/components/InitialLink/index.js
@@ -0,0 +1,17 @@
+// ukljucivanje elemenata potrebnih za kreiranje InitalLogoa
+import React from 'react';
+import { TouchableWithoutFeedback, View, Link } from 'react-native';
+import styles from './styles.js';
+
+// kreiranje komponente sa pripadajucima parametrima
+const InitalLink = ({onPress, type}) => (
+  <TouchableWithoutFeedback onPress={onPress}>
+    <View style={styles.wrapper}>
+      <Text style={styles.message}>
+        {type} is <Text style={styles.messageBold}>is bold</Text>
+      </Text>
+    </View>
+  </TouchableWithoutFeedback>
+)
+
+export default InitalLink;
