9eb3dce2388aa00f36b468362d2bc44d0946fcfc
dbaric <drazen@dump.hr>
2018-01-17 16:47:17 +0100

    :hammer:

--- LiftOff.Mobile/components/Search/index.js
+++ LiftOff.Mobile/components/Search/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles.js';
+import onEnter from './onEnter.js'
 
 const Search = () => (
-  
+  <View style={styles.wrapper}>
+    <TextInput underlineColorAndroid={'transparent'} placeholder="hueheu" style={styles.input} placeholderTextColor="#ccc" onSubmitEditing={() => onEnter())} />
+  </View>
 );
 
 export default Home;
