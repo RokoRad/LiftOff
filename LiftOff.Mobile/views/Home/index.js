acf1d5b99488fd3edfb13d16441f3634adf36bc5
dbaric <drazen@dump.hr>
2018-01-18 18:38:19 +0100

    :hammer:

--- LiftOff.Mobile/views/Home/index.js
+++ LiftOff.Mobile/views/Home/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, AsyncStorage } from 'react-native';
 import signalr from 'react-native-signalr';
-import { broadcast } from '../../functions/realtime';
 import HomeRating from '../../components/HomeRating';
 import HomeList from '../../components/HomeList';
 import Screen from '../../components/Screen';
@@ -10,7 +9,7 @@ import Toast from 'react-native-simple-toast';
 
 const connection = signalr.hubConnection('http://liftoffapi.azurewebsites.net/signalr'),
       proxy = connection.createHubProxy('weatherHub'),
-      units = 'metric'
+      units = 'metric';
 
 // const initial = (object, units) => {
 // proxy.invoke('initiateConnection', object, units);
@@ -47,25 +46,67 @@ class Home extends React.Component {
     }
   };
 
+  // componentWillMount() {
+  //   proxy.on('broadcastWeather', (response) => {
+  //       //radi sta triba sa weatherRatingon
+  //       console.log(response);
+  //   });
+  //   connection.stop();
+  //   connection.start().done(() => {
+  //   console.log("usa")
+  //   proxy.invoke('initiateConnection', {
+  //     location: {
+  //       latitude: 43.508133,
+  //       longitude: 16.440193
+  //     },
+  //     time: new Date().toISOString()
+  //   }, units);
+  //     console.log("prosa")
+  //   }).fail(() => {
+  //     console.log("server error")
+  //   // server error
+  //   });
+  // }
+
   componentWillMount() {
-    proxy.on('broadcastWeather', (response) => {
-        //radi sta triba sa weatherRatingon
-        console.log(response);
-    });
     connection.stop();
-    connection.start().done(() => {
-    console.log("usa")
-    proxy.invoke('initiateConnection', {
-      location: {
-        latitude: 43.508133,
-        longitude: 16.440193
-      },
-      time: new Date().toISOString()
-    }, units);
-      console.log("prosa")
-    }).fail(() => {
-      console.log("server error")
-    // server error
+    AsyncStorage.getItem('@timeLocation').then((value) => {
+      proxy.on('broadcastWeather', (response) => {
+        console.log(response);
+        AsyncStorage.setItem('@realtime', JSON.stringify(response)).then();
+        this.setState({
+          list: response
+        })
+      }); 
+      connection.start().done(() => {
+        if(value !== null) {
+          console.log("value exists")
+          let parsed = JSON.parse(value);
+          proxy.invoke('initiateConnection', {
+            location: {
+              latitude: parsed.location.latitude,
+              longitude: parsed.location.longitude
+            },
+            time: new Date().toISOString()
+          }, units);
+        } else {
+          console.log("value doesnt exist")
+          proxy.invoke('initiateConnection', {
+            location: {
+              latitude: 43.55,
+              longitude: 16.5
+            },
+            time: new Date().toISOString()
+          }, units);
+        }
+      }).fail(() => {
+        AsyncStorage.getItem('@realtime').then((cache) => {
+          this.setState({
+            list: JSON.parse(cache)
+          });
+        });
+        Toast.show("Server error");
+      });
     });
   }
 
