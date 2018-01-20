2e49698c8d6a896c4de2382d19253db90d7eadb7
dbaric <drazen@dump.hr>
2018-01-20 18:05:54 +0100

    :hammer:

--- LiftOff.Mobile/languages/en/Profile/index.js
+++ LiftOff.Mobile/languages/en/Profile/index.js
@@ -1,5 +1,11 @@
 const strings = {
-  userName: '',
+  userName: 'Username',
+  email: 'Email',
+  favoriteFlightSpot: 'Favorite flight spot',
+  favoriteFlightTime: 'Favorite flight time',
+  totalFlights: 'Total flights',
+  totalFlySafeScore: 'Average FlySafe score',
+  totalTimeFlown: 'Total time flown'
 };
 
 export default strings;
--- LiftOff.Mobile/languages/hr/Profile/index.js
+++ LiftOff.Mobile/languages/hr/Profile/index.js
@@ -1,5 +1,11 @@
 const strings = {
-
+  userName: 'Korisničko ime',
+  email: 'Email',
+  favoriteFlightSpot: 'Najčešće mjesto leta',
+  favoriteFlightTime: 'Najčešće vrijeme letenja',
+  totalFlights: 'Ukupno letova',
+  totalFlySafeScore: 'Prosječna FlySafe ocjena',
+  totalTimeFlown: 'Ukupno vrijeme letenja'
 };
 
 export default strings;
