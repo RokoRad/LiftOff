99e2a71cb6c1077efb2433c3c69bee493c217b4b
dbaric <drazen@dump.hr>
2018-01-20 20:17:49 +0100

    :hammer:

--- LiftOff.Mobile/languages/en/index.js
+++ LiftOff.Mobile/languages/en/index.js
@@ -1,5 +1,19 @@
-const strings = {
+import Home from './Home';
+import Initial from './Initial';
+import Map from './Map';
+import Navigation from './Navigation';
+import Profile from './Profile';
+import Settings from './Settings';
+import Stopwatch from './Stopwatch';
 
+const strings = {
+  ...Home,
+  ...Initial,
+  ...Map,
+  ...Navigation,
+  ...Profile,
+  ...Settings,
+  ...Stopwatch
 };
 
 export default strings;
