d05c5441376503e2f22acffed69e0b83cb6a3727
dbaric <drazen@dump.hr>
2018-01-20 17:53:36 +0100

    :hammer:

--- LiftOff.Mobile/languages/en/Home/index.js
+++ LiftOff.Mobile/languages/en/Home/index.js
@@ -1,5 +1,20 @@
 const strings = {
-
+  WeatherRating: 'Weather rating',
+  Atmosphere: 'Atmosphere',
+  Humidity: 'Humidity',
+  Pressure: 'Pressure',
+  Conditions: 'Conditions',
+  Weather: 'Weather',
+  Description: 'Description',
+  Temperature: 'Temperature',
+  Min_Temperature: 'Min temperature',
+  Max_Temperature: 'Max temperature',
+  Uv: 'UV',
+  Cloudiness: 'Cloudiness',
+  Visibility: 'Visibility',
+  Wind: 'Wind',
+  WindDirection: 'Wind direction',
+  WindSpeed: 'Wind speed'
 };
 
 export default strings;
--- LiftOff.Mobile/languages/en/Navigation/index.js
+++ LiftOff.Mobile/languages/en/Navigation/index.js
@@ -1,5 +1,9 @@
 const strings = {
-
+  FlySafe: 'FlyeSafe',
+  Planning: 'Planning',
+  LiftOff: 'LiftOff',
+  Profile: 'Profile',
+  Settings: 'Settings'
 };
 
 export default strings;
