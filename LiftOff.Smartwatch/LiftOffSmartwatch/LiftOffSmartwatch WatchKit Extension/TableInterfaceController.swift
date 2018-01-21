9f94436a7553a9da55a5ab15368866967cdbb999
Tpuljak <toma.puljak@hotmail.com>
2018-01-21 20:12:14 +0100

    Minor fixes

--- LiftOff.Smartwatch/LiftOffSmartwatch/LiftOffSmartwatch WatchKit Extension/TableInterfaceController.swift
+++ LiftOff.Smartwatch/LiftOffSmartwatch/LiftOffSmartwatch WatchKit Extension/TableInterfaceController.swift
@@ -11,12 +11,14 @@ class TableInterfaceController: WKInterfaceController {
         //Slijedeći nam kod uzima vrijednosti koje je glavno sučelje poslalo i formulira tablicu ocjena
         if let newContext = context as? [NSObject] {
             if (newContext.count != 0) {
+                
                 self.Table.setNumberOfRows(newContext.count, withRowType: "ScoreRow")
                 for i in 0...newContext.count - 1 {
+                    print(newContext[i])
                     let row = Table.rowController(at: i) as! TableRow
                     row.CategoryLabel.setText(newContext[i].value(forKey: "category") as? String ?? "no category")
                     row.ScoreLabel.setText(newContext[i].value(forKey: "score") as? String ?? "/")
-                    row.RowGroup.setBackgroundColor(UIColor.color(fromHexString: GetColor(score: newContext[i].value(forKey: "score") as? Double ?? 5.0)))
+                    row.RowGroup.setBackgroundColor(UIColor.color(fromHexString: TableInterfaceController.GetColor(score: newContext[i].value(forKey: "score") as? Double ?? 5.0)))
                 }
             }
         }
@@ -27,12 +29,12 @@ class TableInterfaceController: WKInterfaceController {
     //Pomoćna funkcija koja određuje boju reda tablice ovisno o ocjeni
     static func GetColor (score: Double) -> String {
         if(score < 2.0) {
-            return "0xF95F62"
+            return "#F95F62"
         }
         else if (score < 4.0) {
-            return "0xFF9052"
+            return "#FF9052"
         }
-        return "0x47e389"
+        return "#47e389"
     }
     
     //Funkcija koja se pokreće pri aktivaciji sučelja
