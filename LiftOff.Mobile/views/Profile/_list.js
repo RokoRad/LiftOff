ca0b0a1a056b95788bd3084c89a6a9c24c6806c8
dbaric <drazen@dump.hr>
2018-01-23 16:05:35 +0100

    :hammer:

--- LiftOff.Mobile/views/Profile/_list.js
+++ LiftOff.Mobile/views/Profile/_list.js
@@ -4,7 +4,7 @@ import language from '../../languages';
 
 export default (object) => {
   Object.entries(object).forEach(([key, value]) => {
-    console.log(key, value)
+    console.log(key, value);
     <AccountItem title={language[key]} content={value} />
   });
 };
