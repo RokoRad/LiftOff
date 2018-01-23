611cc5a79cf8b5c2830bcfda4f4c432566d3ef7b
dbaric <drazen@dump.hr>
2018-01-23 13:18:11 +0100

    :hammer:

--- LiftOff.Mobile/views/Profile/index.js
+++ LiftOff.Mobile/views/Profile/index.js
@@ -15,52 +15,29 @@ class Account extends Component {
     super(props);
   };
 
-  // poziv prilikom renderiranja komponente
-  // componentWillMount() {
-  //   // povlačenje vrijednosti iz cachea
-  //   // dohvaćanje tokena i postavljanje vrijednosti dobivene sa servera
-  //   AsyncStorage.getItem('@token').then((value) => {
-  //     fetch('http://liftoffapi.azurewebsites.net/api/account/getUserData', {
-  //       method: 'GET',
-  //       headers: {
-  //         'Authorization': 'Bearer ' + value
-  //       }
-  //     }).then((response) => {
-  //       // provjera povratnog statusa servera
-  //       if(response.status === 200) {
-  //         // updateanje vrijednosti i cachranje
-  //         this.setState({
-  //           userObject: JSON.parse(response._bodyInit)
-  //         });
-  //         AsyncStorage.setItem('@stats', (response._bodyInit));
-  //       } else if (response.status === 401) {
-  //         // za invalidan token, povratak na login
-  //         removeToken();
-  //         this.props.history.push('/');
-  //       }
-  //     })
-  //   })
-  // }
-  
   // funckija renderiranja te sadržaj komponente
   render() {
+    console.log(this.props)
     return (
       <Screen current={this.props.location}>
         <AccountMap moreThan={language.moreThan} flewHere={language.flewHere} />
         <View style={styles.container}>
-          <AccountItem title={language.userName} content={this.props.userName} />
-          <AccountItem title={language.email} content={this.props.email} />
-          <AccountItem title={language.favoriteFlightSpot} content={this.props.favoriteFlightSpot} />
-          <AccountItem title={language.favoriteFlightTime} content={this.props.favoriteFlightTime} />
-          <AccountItem title={language.totalFlights} content={this.props.totalFlights} />
-          <AccountItem title={language.totalFlySafeScore} content={this.props.totalFlySafeScore} />
-          <AccountItem title={language.totalTimeFlown} content={this.props.totalTimeFlown} />
+          {this.props.stats.map((value) => (
+            console.log(value)
+            //<AccountItem title={language[value]} content={value}/>
+          ))}
+          {/* <AccountItem title={language.userName} content={this.props.stats.userName} />
+          <AccountItem title={language.email} content={this.props.stats.email} />
+          <AccountItem title={language.favoriteFlightSpot} content={this.props.stats.favoriteFlightSpot} />
+          <AccountItem title={language.favoriteFlightTime} content={this.props.stats.favoriteFlightTime} />
+          <AccountItem title={language.totalFlights} content={this.props.stats.totalFlights} />
+          <AccountItem title={language.totalFlySafeScore} content={this.props.stats.totalFlySafeScore} />
+          <AccountItem title={language.totalTimeFlown} content={this.props.stats.totalTimeFlown} /> */}
         </View>
       </Screen>
     );
   }
 }
-
 
 const mapStateToProps = state => ({
   ...state.profileReducer
