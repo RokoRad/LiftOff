d6e1f78821fc8795fcfbff1ba3ac44405915d6ab
dbaric <drazen@dump.hr>
2018-01-23 16:29:29 +0100

    :hammer:

--- LiftOff.Mobile/views/Profile/_list.js
+++ LiftOff.Mobile/views/Profile/_list.js
@@ -3,8 +3,23 @@ import AccountItem from '../../components/AccountItem';
 import language from '../../languages';
 
 export default (object) => {
-  Object.entries(object).forEach(([key, value]) => {
-    console.log(key, value);
-    <AccountItem title={language[key]} content={value} />
-  });
+  let key = Object.keys(object);
+  let value = Object.values(object);
+  for(let i = 0; i < key.length; i++) {
+    return <AccountItem title={key[i]} content={value[i]} />
+  }
+  // console.log(object)
+  // console.log(Object.keys(object)[1])
+  // console.log(object[1])
+  // object.map((item) => {
+  //   console.log(item)
+  // })
+  //console.log(object)
+  // Object.keys(object).map((item) => {
+  //   console.log(item, item[item])
+  // });
+  // Object.entries(object).forEach(([key, value]) => {
+  //   console.log(key, value);
+  //   <AccountItem title={language[key]} content={value} />
+  // });
 };
