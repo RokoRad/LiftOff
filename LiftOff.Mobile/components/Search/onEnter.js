b15288a1532387832dfb8ac03b4195584a41a264
dbaric <drazen@dump.hr>
2018-01-24 13:11:22 +0100

    :hammer:

--- LiftOff.Mobile/components/Search/onEnter.js
+++ LiftOff.Mobile/components/Search/onEnter.js
@@ -6,10 +6,11 @@ import key from '../../config/googleKey.js';
 export default (value) => {
   console.log(value)
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${value}&key=${key}`).then((response) => {
-    const data = JSON.parse(response._bodyInit).data.results[0].geometry.location;
+    const data = JSON.parse(response._bodyInit),
+          parsed = data.results[0].geometry.location;
     store.dispatch(updateLocation({
-      latitude: data.lat,
-      longitude: data.lng
+      latitude: parsed.lat,
+      longitude: parsed.lng
     }));
     // ref.animateToCoordinate({
     //   ...location
