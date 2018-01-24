cf2ebecc654f86eef82a8ddf17f1809ceadd6115
dbaric <drazen@dump.hr>
2018-01-24 14:35:51 +0100

    :hammer:

--- LiftOff.Mobile/languages/index.js
+++ LiftOff.Mobile/languages/index.js
@@ -1,13 +1,25 @@
 import { AsyncStorage } from 'react-native';
 import croatian from './hr';
 import english from './en';
+
+let language;
+
 // provjera jezika
 AsyncStorage.getItem('@language').then((response) => {
+  console.log(response);
     if(response === 'hr') {
-      module.exports.default = croatian;
+      module.exports.default = {
+        ...croatian,
+        language: 'hr'
+      }
     } else {
-      module.exports.default = english;
+      module.exports.default = {
+        ...english,
+        language: 'en'
+      };
       // ako je prvi render
       AsyncStorage.setItem('@language', 'en');
     }
 });
+
+export default language;
