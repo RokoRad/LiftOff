7e3fb2bf2ba8ad01dd13be1bc2e36c36e113d335
dbaric <drazen@dump.hr>
2018-01-24 19:15:40 +0100

    :nail_care:

--- LiftOff.Mobile/components/Search/styles.js
+++ LiftOff.Mobile/components/Search/styles.js
@@ -10,19 +10,18 @@ const styles = StyleSheet.create({
     paddingLeft: 15,
     paddingRight: 15,
     backgroundColor: '#fff',
-    zIndex: 999,
-    borderRadius: 5,    
-    shadowOpacity: 0.5,
+    zIndex: 999,    
+    shadowOpacity: 0.1,
     shadowRadius: 5,
     shadowOffset: {
-        height: 12,
-        width: 12
+        height: 10,
+        width: 10
     },
-    elevation: 12
+    elevation: 15
   },
   input: {
-    fontSize: 12,
-    fontFamily: 'barlowMedium',
+    fontSize: 14,
+    fontFamily: 'robotoLight',
     color: '#939393'
   },
   out: {
