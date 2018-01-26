fa71fdc817d4aaf01701e4fc05502495f0f50908
dbaric <drazen@dump.hr>
2018-01-26 21:03:45 +0100

    :nail_care:

--- LiftOff.Mobile/views/Map/_currentTime.js
+++ LiftOff.Mobile/views/Map/_currentTime.js
@@ -1,3 +1,13 @@
 export default () => {
-  return `${new Date().getHours()}:${new Date().getMinutes()}`
+  let now = new Date(),
+      hours = now.getHours(),
+      minutes = now.getMinutes();
+
+  if(hours < 10) {
+    hours = `0${hours}`
+  } else if (minutes < 10) {
+    minutes = `0${hours}`
+  }
+
+  return `${hours}:${minutes}`
 }
