76400562062e3ef705f183dd981fa0d1e0fd283a
dbaric <drazen@dump.hr>
2018-01-27 23:37:24 +0100

    :hammer:

--- LiftOff.Mobile/views/Home/styles.js
+++ LiftOff.Mobile/views/Home/styles.js
@@ -1,13 +1,23 @@
 import { StyleSheet } from 'react-native';
+import vars from '../../config/vars.js';
 
 export default StyleSheet.create({
   wrapper: {
-    backgroundColor: 'blue'
+    display: 'flex',
+    width: '70%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    opacity: 0.7,
+    marginVertical: 10
   },
   left: {
-
+    fontFamily: vars.regular,
+    fontSize: 14
   },
   right: {
-    
+    fontFamily: vars.regular,
+    fontSize: 14
   }
 })
