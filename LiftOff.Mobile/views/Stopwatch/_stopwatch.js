96eba7c92340ba70aaa10e1af83e6d45907b1750
dbaric <drazen@dump.hr>
2018-01-28 17:50:36 +0100

    :hammer:

--- LiftOff.Mobile/views/Stopwatch/_stopwatch.js
+++ LiftOff.Mobile/views/Stopwatch/_stopwatch.js
@@ -15,42 +15,46 @@ import headers from '../../functions/headers';
 export default (history, drone) => {
   let state = store.getState().stopwatchReducer.stopwatch,
     home = store.getState().homeReducer.home,
-    location = home.weatherData.TimeLocation.Location,
+    // location = home.weatherData.TimeLocation.Location,
     flightSpot = home.weatherData.City,
     flySafeScore = home.TotalRating;
 
   if (state.active) {
-    AsyncStorage.getItem('@token').then(token => {
-      fetch('http://liftoffapi.azurewebsites.net/api/logging/logFlight', {
-        method: 'POST',
-        headers: headers(token),
-        body: JSON.stringify({
-          timeFlown: _timeFlown(state.minutes, state.seconds),
-          flySafeScore,
-          drone: {
-            name: drone
-          },
-          flightLocation: {
-            flightSpot,
-            latitude: location.Latitude,
-            longitude: location.Longitude
-          },
-          flightTime: {
-            flightStartTime: state.startTime
-          }
-        })
-      }).then(response => {
-        store.dispatch(setStarttime(''));
-        store.dispatch(updateSeconds((state.seconds = 0)));
-        store.dispatch(updateMinutes((state.minutes = 0)));
-        if (response.status === 200) {
-          store.dispatch(updateStats(JSON.parse(response._bodyInit)));
-          AsyncStorage.setItem('@stats', response._bodyInit);
-        } else if (response.status === 401) {
-          removeToken(history);
-        }
-      });
-    });
+    // AsyncStorage.getItem('@token').then(token => {
+    //   fetch('http://liftoffapi.azurewebsites.net/api/logging/logFlight', {
+    //     method: 'POST',
+    //     headers: headers(token),
+    //     body: JSON.stringify({
+    //       timeFlown: _timeFlown(state.minutes, state.seconds),
+    //       flySafeScore,
+    //       drone: {
+    //         name: drone
+    //       },
+    //       flightLocation: {
+    //         flightSpot,
+    //         latitude: location.Latitude,
+    //         longitude: location.Longitude
+    //       },
+    //       flightTime: {
+    //         flightStartTime: state.startTime
+    //       }
+    //     })
+    //   }).then(response => {
+    //     store.dispatch(setStarttime(''));
+    //     store.dispatch(updateSeconds((state.seconds = 0)));
+    //     store.dispatch(updateMinutes((state.minutes = 0)));
+    //     if (response.status === 200) {
+    //       store.dispatch(updateStats(JSON.parse(response._bodyInit)));
+    //       AsyncStorage.setItem('@stats', response._bodyInit);
+    //     } else if (response.status === 401) {
+    //       removeToken(history);
+    //     }
+    //   });
+    // });
+
+    store.dispatch(setStarttime(''));
+    store.dispatch(updateSeconds((state.seconds = 0)));
+    store.dispatch(updateMinutes((state.minutes = 0)));
     clearInterval(this.stopwatch);
     store.dispatch(
       addLog({
@@ -58,6 +62,7 @@ export default (history, drone) => {
         rating: flySafeScore
       })
     );
+
   } else {
     store.dispatch(setStarttime((state.startTime = new Date().toISOString())));
     this.stopwatch = setInterval(() => {
