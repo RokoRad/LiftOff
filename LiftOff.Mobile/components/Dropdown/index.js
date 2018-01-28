387fd89bc6598d59770a1534895ac971609cf46d
dbaric <drazen@dump.hr>
2018-01-28 13:32:56 +0100

    :hammer:

--- LiftOff.Mobile/components/Dropdown/index.js
+++ LiftOff.Mobile/components/Dropdown/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Picker } from 'react-native';
+import _changeDrone from './_changeDrone.js';
 import styles from './styles.js';
 
 class Dropdown extends Component {
@@ -9,15 +10,17 @@ class Dropdown extends Component {
 
   render() {
     return ( 
-      <Picker selectedValue="DJI_Spark" onValueVhange={() => console.log("aa")} style={styles.picker}>
-        <Picker.Item label="DJI Phantom 4 Pro" value="DJI_Phantom_4_Pro" />
-        <Picker.Item label="DJI Phantom 4 Advanced" value="DJI_Phantom_4_Advanced" />
-        <Picker.Item label="DJI Phantom 3 SE" value="DJI_Phantom_3_SE" />
-        <Picker.Item label="DJI Mavic Air" value="DJI_Mavic_Air" />
-        <Picker.Item label="DJI Mavic Pro" value="DJI_Mavic_Pro" />
-        <Picker.Item label="DJI Spark" value="DJI_Spark" />
-        <Picker.Item label="Yuneec Typhoon H Pro" value="Yuneec_Typhoon_H_pro" />
+      <Picker selectedValue="DJI Spark" onValueVhange={(value) => console.log(value)} style={styles.picker}>
+        <Picker.Item label="DJI Phantom 4 Pro" value="DJI Phantom 4 Pro" />
+        <Picker.Item label="DJI Phantom 4 Advanced" value="DJI Phantom 4 Advanced" />
+        <Picker.Item label="DJI Phantom 3 SE" value="DJI Phantom 3 SE" />
+        <Picker.Item label="DJI Mavic Air" value="DJI Mavic Air" />
+        <Picker.Item label="DJI Mavic Pro" value="DJI Mavic Pro" />
+        <Picker.Item label="DJI Spark" value="DJI Spark" />
+        <Picker.Item label="Yuneec Typhoon H Pro" value="Yuneec Typhoon H pro" />
       </Picker>
     )
   }
 }
+
+export default Dropdown;
