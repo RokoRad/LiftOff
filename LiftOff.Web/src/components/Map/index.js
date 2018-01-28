1d5f3058dfffaee9db1685ed09b5489a5e86788d
dbaric <drazen@dump.hr>
2018-01-28 21:05:07 +0100

    :hammer:

--- LiftOff.Web/src/components/Map/index.js
+++ LiftOff.Web/src/components/Map/index.js
@@ -4,23 +4,21 @@ import './style.css';
 
 var ReactGoogleMaps = require('react-googlemaps');
 var GoogleMapsAPI = window.google.maps;
- 
+
 var Map = ReactGoogleMaps.Map;
 var Marker = ReactGoogleMaps.Marker;
 var OverlayView = ReactGoogleMaps.OverlayView;
 
 export default ({}) => (
   <div className="map">
- <Map
- initialZoom={10}
- initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-
- <OverlayView
-   style={{backgroundColor: '#fff'}}
-   position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-   <p>Some content</p>
- </OverlayView>
-</Map>
+    <Map initialZoom={10} initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
+      <OverlayView
+        style={{ backgroundColor: '#fff' }}
+        position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}
+      >
+        <p>Some content</p>
+      </OverlayView>
+    </Map>
     <Dock />
   </div>
 );
