2bbcb669b1362a73d42377dda1c5a20bd098b6d3
dbaric <drazen@dump.hr>
2018-01-28 19:52:45 +0100

    :hammer:

--- LiftOff.Web/src/components/Graph/index.js
+++ LiftOff.Web/src/components/Graph/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Chart from 'react-chartjs2';
 import './style.css';
 
-export default () => <div className="graph">graph</div>;
+export default () => (
+  <div className="graph">
+    <Chart data={null} options={null} type='line' />
+  </div>
+);
