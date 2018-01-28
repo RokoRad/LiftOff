d06aae6af30227f15784e576e14d274c00f7ea96
dbaric <drazen@dump.hr>
2018-01-28 20:03:02 +0100

    :hammer:

--- LiftOff.Web/src/components/Graph/index.js
+++ LiftOff.Web/src/components/Graph/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import Chart from 'react-chartjs2';
 import './style.css';
 
+var data = {
+  labels: [1, 2, 3, 4, 5],
+  datasets: [{ 
+      data: [0.1, 2.3, 4.6, 4.2, 1.5],
+      borderColor: "#3e95cd",
+      fill: false
+    }
+  ]
+};
+
+var options = {
+  legend: { display: false },
+  responsive: true
+}
+
 export default () => (
   <div className="graph">
-    <Chart data={null} options={null} type='line' />
+    <Chart data={data} options={options} type='line' />
   </div>
 );
