d5053cef4d8c5931dd9df1ad4144cf3f481d0218
dbaric <drazen@dump.hr>
2018-01-28 21:38:24 +0100

    :hammer:

--- LiftOff.Web/src/components/Map/index.js
+++ LiftOff.Web/src/components/Map/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import Dock from '../Dock';
 import './style.css';
+import mapStyle from './style.js';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-var ReactGoogleMaps = require('react-googlemaps');
-var GoogleMapsAPI = window.google.maps;
+class MapContainer extends React.Component {
+  render() {
+    return (
+      <div className="map">
+          <Map google={this.props.google} zoom={14} disableDefaultUI={true} styles={mapStyle}>
 
-var Map = ReactGoogleMaps.Map;
-var Marker = ReactGoogleMaps.Marker;
-var OverlayView = ReactGoogleMaps.OverlayView;
+            <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
 
-export default ({}) => (
-  <div className="map">
-    <Map initialZoom={10} initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-      <OverlayView
-        style={{ backgroundColor: '#fff' }}
-        position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}
-      >
-        <p>Some content</p>
-      </OverlayView>
-    </Map>
-    <Dock />
-  </div>
-);
+            <InfoWindow onClose={this.onInfoWindowClose}>
+                <div>
+                  <h1>aa</h1>
+                </div>
+            </InfoWindow>
+          </Map>
+        <Dock />
+      </div>
+    )
+  }
+};
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyB72p06i9XP1ypkoDfhaGgF3aK8bFg3AHY'
+})(MapContainer)
