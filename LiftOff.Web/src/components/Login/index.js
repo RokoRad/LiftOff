fb87d0772051fa40ba0fa054e77b3cc06e01b5e8
dbaric <drazen@dump.hr>
2018-01-28 19:24:21 +0100

    :hammer:

--- LiftOff.Web/src/components/Login/index.js
+++ LiftOff.Web/src/components/Login/index.js
@@ -3,6 +3,7 @@ import Input from '../Input';
 import Button from '../Button';
 import InitalMessage from '../InitalMessage';
 import storage from '../../functions/storage';
+import _login from './_login.js';
 import './style.css';
 
 let holder = {
@@ -10,25 +11,12 @@ let holder = {
   password: null
 };
 
-const login = () => {
-  // axios({
-  //   method: 'POST',
-  //   url: 'http://liftoffapi.azurewebsites.net/api/account/register',
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   data: holder
-  // }).then((response) => {
-  //   console.log(response)
-  // });
-};
-
 const Login = () => (
   <form className="login">
     <Input placeholder="Username" onChange={() => holder.username} />
     <Input placeholder="Password" type="password" minLength="8" onChange={() => holder.password} />
     <InitalMessage type="login" />
-    <Button onClick={() => login()}>Login</Button>
+    <Button onClick={() => _login()}>Login</Button>
   </form>
 );
 
--- LiftOff.Web/src/components/Register/index.js
+++ LiftOff.Web/src/components/Register/index.js
@@ -2,25 +2,13 @@ import React from 'react';
 import Input from '../Input';
 import Button from '../Button';
 import InitalMessage from '../InitalMessage';
+import _register from './_register.js';
 import './style.css';
 
 let holder = {
   username: null,
   email: null,
   password: null
-};
-
-const register = () => {
-  // axios({
-  //   method: 'POST',
-  //   url: 'http://liftoffapi.azurewebsites.net/api/account/register',
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   data: holder
-  // }).then((response) => {
-  //   console.log(response)
-  // });
 };
 
 const Login = () => (
@@ -34,7 +22,7 @@ const Login = () => (
       onChange={e => (holder.password = e)}
     />
     <InitalMessage type="register" />
-    <Button onClick={() => register()}>Register</Button>
+    <Button onClick={() => _register()}>Register</Button>
   </form>
 );
 
