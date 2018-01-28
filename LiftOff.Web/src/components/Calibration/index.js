a32642fd9a20bc142ab3165bcdc0f5543ee05d78
dbaric <drazen@dump.hr>
2018-01-28 21:51:11 +0100

    :hammer:

--- LiftOff.Web/src/components/Calibration/index.js
+++ LiftOff.Web/src/components/Calibration/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import _calibration from './_calibration.js';
 import './style.css';
 
 export default ({}) => (
-  <div className="calibration">a</div>
+  <div className="calibration" onClick={() => _calibration()}></div>
 );
--- LiftOff.Web/src/components/Dock/index.js
+++ LiftOff.Web/src/components/Dock/index.js
@@ -3,4 +3,9 @@ import Calibration from '../Calibration';
 import Picker from '../Picker';
 import './style.css';
 
-export default ({}) => <div className="dock" />;
+export default ({}) => (
+  <div className="dock">
+    <Calibration />
+    <Picker />
+  </div> 
+);
