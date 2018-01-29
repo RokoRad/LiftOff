972e51a09e696ede0470956bb7a324ee35240e76
dbaric <drazen@dump.hr>
2018-01-29 17:38:43 +0100

    :hammer:

--- LiftOff.Web/src/components/HomeText/index.js
+++ LiftOff.Web/src/components/HomeText/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 export default ({ }) => (
   <div className="home__text">
-    <div className="home-text__title">big title</div>
-    <div className="home-text__message">long text</div>
+    <div className="home-text__title">Weather rating</div>
+    <div className="home-text__message">lorem lorem hahah lorem lorem hahah lorem lorem hahah lorem lorem hahah lorem lorem hahah lorem lorem hahah</div>
   </div>
 );
