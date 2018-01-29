951d8f9818bc866a84c36b841aefd2d7cc7c3e74
dbaric <drazen@dump.hr>
2018-01-29 18:23:04 +0100

    :hammer:

--- LiftOff.Web/src/components/Home/index.js
+++ LiftOff.Web/src/components/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomeRating from '../HomeRating';
 import HomeList from '../HomeList';
+import colorGenerator from '../../functions/colorGenerator';
 import './style.css';
 
 export default ({ }) => (
   <div className="home">
-    <HomeRating color="red" />
+    <HomeRating color={colorGenerator(2.3)} />
     <HomeList />
   </div>
 );
