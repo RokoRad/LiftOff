9525e36182d00e9b899d7504bece55831aee3df8
dbaric <drazen@dump.hr>
2018-01-29 18:24:30 +0100

    :hammer:

--- LiftOff.Web/src/components/HomeText/index.js
+++ LiftOff.Web/src/components/HomeText/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './style.css';
+import language from '../../languages';
 
 export default ({ }) => (
   <div className="home__text">
-    <div className="home-text__title">Weather rating</div>
+    <div className="home-text__title">{language.WeatherRating}</div>
     <div className="home-text__message">lorem lorem hahah lorem lorem hahah lorem lorem hahah lorem lorem hahah lorem lorem hahah lorem lorem hahah</div>
   </div>
 );
