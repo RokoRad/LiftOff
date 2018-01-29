c90532c7c8f0c32bbe96648a4896d5bb3f267353
dbaric <drazen@dump.hr>
2018-01-29 17:29:45 +0100

    :hammer:

--- LiftOff.Web/src/components/HomeRating/index.js
+++ LiftOff.Web/src/components/HomeRating/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Drone from '../Drone';
+import HomeText from '../HomeText';
 import './style.css';
 
 export default ({ }) => (
   <div className="home__rating">
-
+    <Drone />
+    <HomeText />
   </div>
 );
---
+++ LiftOff.Web/src/components/HomeText/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+export default ({ }) => (
+  <div className="home__text">
+    <div className="home-text__title">big title</div>
+    <div className="home-text__message">long text</div>
+  </div>
+);
