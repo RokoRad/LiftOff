d2d5b4731c0a750dac09b14d4567cc958ca058eb
dbaric <drazen@dump.hr>
2018-01-29 21:26:22 +0100

    :hammer:

--- LiftOff.Web/src/components/Picker/_onSelect.js
+++ LiftOff.Web/src/components/Picker/_onSelect.js
@@ -2,7 +2,6 @@ import store from '../../store';
 import { updateDateTime } from '../../actions';
 
 export default (e) => {
-  console.log(new Date(e).toISOString())
-  //store.dispatch(updateDateTime((store.getState().timeLocationReducer.timeLocation.time = value)));
+  store.dispatch(updateDateTime((store.getState().timeLocationReducer.timeLocation.time = new Date(e).toISOString())));
   
 }
