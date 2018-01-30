18e7216bc55c684ad05c87d22eeaf4cb602e901e
dbaric <drazen@dump.hr>
2018-01-30 20:36:39 +0100

    :hammer:

--- LiftOff.Web/src/components/Search/index.js
+++ LiftOff.Web/src/components/Search/index.js
@@ -6,4 +6,4 @@ import _onEnter from './_onEnter.js';
 
 let value = '';
 
-export default ({  }) => <input className="search" placeholder={capitalize(language.Search)} onKeyPress={(e) => _onEnter(e, value)} onChange={(e) => value = e}/>;
+export default ({  }) => <input className="search" placeholder={capitalize(language.Search)} onKeyPress={(e) => _onEnter(e, value)} onChange={(e) => value = e.target.value}/>;
