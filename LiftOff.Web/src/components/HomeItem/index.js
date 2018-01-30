dde24604fb71affa4631c49d74534c996e972d19
dbaric <drazen@dump.hr>
2018-01-30 13:16:13 +0100

    :hammer:

--- LiftOff.Web/src/components/HomeItem/index.js
+++ LiftOff.Web/src/components/HomeItem/index.js
@@ -1,6 +1,48 @@
 import React from 'react';
 import './style.css';
+import language from '../../language';
+import capitalize from '../../functions/capitalize';
+import isValueText from '../../functions/isValueText';
 
-export default ({}) => (
+const icons = {
+  atmosphere: '../../images/weather/atmosphere.png',
+  conditions: '../../images/weather/conditions.png',
+  temperature: '../../images/weather/temperature.png',
+  uv: '../../images/weather/uv.png',
+  visibility: '../../images/weather/visibility.png',
+  wind: '../../images/weather/wind.png'
+};
 
+export default ({ type, fVal, sVal, fName, sName, fAddon, sAddon, rating }) => (
+  <div className="home-item__wrapper">
+    <div className="home-item__left home-item__aligned">
+      <img src={`${icons[type]}`} className="home-item__icon" />
+    </div>
+    <div className="home-item__middle">
+      <div>
+        <span className="home-item__title">{language[capitalize(type)]}</span>
+      </div>
+      <div>
+        <div className="home-item__row">
+          <span className="home-item__left-span">{language[fName]}</span>
+          <span className="home-item__right-span">
+            {isValueText(fVal)}
+            {fAddon}
+          </span>
+        </div>
+        <div className="home-item__row">
+          <span className="home-item__left-span">{language[sName]}</span>
+          <span className="home-item__right-span">
+            {isValueText(sVal)}
+            {sAddon}
+          </span>
+        </div>
+      </div>
+    </div>
+    <div className={[classNames.right, classNames.bothAligned]}>
+      <span className={[styles.rating, rating !== null ? globals[colorGenerator(rating)] : null]}>
+        {isValueText(rating)}
+      </span>
+    </div>
+  </div>
 );
--- LiftOff.Web/src/components/HomeList/index.js
+++ LiftOff.Web/src/components/HomeList/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import HomeItem from '../HomeItem';
 import './style.css';
 
 export default ({ }) => (
   <div className="home__list">
-
+      <HomeItem
+        type="conditions"
+        //rating={list.ConditionsRating}
+        raiting={3.444444}
+        fName="Weather"
+        //fVal={nester.Weather}
+        fVal="Split"
+        sName="Description"
+        //sVal={nester.WeatherDescription}
+        sVal="opis"
+      />
   </div>
 );
