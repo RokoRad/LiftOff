36443263b93df50bc6edee73d7e4ad189e3cc340
dbaric <drazen@dump.hr>
2018-01-31 19:56:06 +0100

    :nail_care:

--- LiftOff.Web/src/components/Links/index.js
+++ LiftOff.Web/src/components/Links/index.js
@@ -4,6 +4,11 @@ import './style.css';
 
 export default ({  }) => (
   <div className="links">
-    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona nešto nešto" />
+    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
+    <Link type="pdf" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
+    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
+    <Link type="pdf" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
+    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
+    <Link type="pdf" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
   </div>
 );
