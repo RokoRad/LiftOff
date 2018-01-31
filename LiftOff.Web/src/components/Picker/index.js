4638bca9edb9bcdc6bb785a302eb3f1ef53509b9
dbaric <drazen@dump.hr>
2018-01-31 22:13:21 +0100

    :nail_care:

--- LiftOff.Web/src/components/Picker/index.js
+++ LiftOff.Web/src/components/Picker/index.js
@@ -3,13 +3,21 @@ import DateTimePicker from 'material-ui-datetimepicker';
 import DatePickerDialog from 'material-ui/DatePicker/DatePickerDialog';
 import TimePickerDialog from 'material-ui/TimePicker/TimePickerDialog';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import createMuiTheme from 'material-ui/styles/theme';
+import createPalette from 'material-ui/styles/palette';
 import _onOpen from './_onOpen.js';
 import _onSelect from './_onSelect.js';
 import './style.css';
 
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary2Color: "#297CCA"
+  })
+})
+
 export default ({}) => (
   <div className="picker">
-    <MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
       <DateTimePicker
         clearIcon={true}
         className="date-picker"
