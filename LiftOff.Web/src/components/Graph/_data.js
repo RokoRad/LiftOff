36f590bb7458503cd18e79e0390b1c535514c03d
dbaric <drazen@dump.hr>
2018-01-31 18:11:11 +0100

    :nail_care:

--- LiftOff.Web/src/components/Graph/_data.js
+++ LiftOff.Web/src/components/Graph/_data.js
@@ -1,17 +1,4 @@
 export default (days, scores) => {
-  let object = [];
-
-  for(let i = 0; i < scores.length; i++) {
-    object.push(scores[i])
-    if(i === 0) {
-
-    } else if (i === 4) {
-      object.push(scores[4]);  
-      object.push(scores[4]);  
-    }
-  }
-
-  console.log(object)
    return {
     labels: days,
     datasets: [
