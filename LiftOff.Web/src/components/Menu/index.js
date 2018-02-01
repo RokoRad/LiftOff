42f02fcce1fbdaacf2ad8dc2f49d62fbab1b7c60
dbaric <drazen@dump.hr>
2018-02-01 21:55:09 +0100

    :nail_care:

--- LiftOff.Web/src/components/Menu/index.js
+++ LiftOff.Web/src/components/Menu/index.js
@@ -9,15 +9,23 @@ import './style.css';
 
 export default ({}) => (
   <div className="menu">
-    <div onClick={() => _changeUnits()}>changeUnits</div>
-    <div onClick={() => _changeLanguage()}>changeLanguage</div>
     <div onClick={() => _closeMenu()} className="menu__close">X</div>
 
+    <div className="memu__item">
+      <span className="memu-item__text">Change units</span>
+      <label class="switch" onClick={() => _changeUnits()}>
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+    </div>
 
-    <label class="switch">
-  <input type="checkbox" />
-  <span class="slider round"></span>
-</label>
+    <div className="memu__item">
+      <span className="memu-item__text">Change language</span>
+      <label class="switch" onClick={() => _changeLanguage()}>
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+    </div>
 
 
     <Button onClick={() => removeToken()}>{language.Logout}</Button>
