4679de1cd313e709791f8f8d4bdaf9625b481607
dbaric <drazen@dump.hr>
2018-02-02 15:37:45 +0100

    :nail_care:

--- LiftOff.Mobile/views/Map/index.js
+++ LiftOff.Mobile/views/Map/index.js
@@ -40,14 +40,25 @@ class Map extends Component {
           cacheEnabled={true}
           showsCompass={false}
           showsScale={false}
-          maxZoom={12}
+          ref={map => { this.map = map }}
         >
-          <Marker
+          {this.map.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: this.props.map,
+            radius: 30
+          })
+          }
+          {/* <Marker
             location={this.props.markerPosition}
             city={this.props.tooltip.city}
             time={_currentTime()}
             rating={this.props.tooltip.rating}
-          />
+          /> */}
           <Marker
             location={this.props.markerPosition}
             city={this.props.tooltip.city}
