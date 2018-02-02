a2c0481aecf4479404fa1e0e1bbd8182feebe911
roko <rokorad@gmail.com>
2018-02-02 13:04:08 +0100

    doda smartwatch pairin

---
+++ LiftOff.API/Controllers/SmartwatchController.cs
@@ -0,0 +1,37 @@
+﻿using LiftOff.API.Logic;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LiftOff.API.Controllers
+{
+    
+    [RoutePrefix("api/smartwatch")]
+    public class SmartwatchController : ApiController
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("registerDevice")]
+        public void RegisterDevice(JObject json)
+        {
+            var deviceID = (string)json["deviceID"];
+            var token = (string)json["token"];
+
+            SmartwatchPairer.Instance.RegisterMobileDevice(deviceID, token);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("getDeviceToken")]
+        public object GetDeviceToken(JObject json)
+        {
+            var deviceID = (string)json["deviceID"];
+
+            return new { token = SmartwatchPairer.Instance.GetRegisteredMobileDeviceToken(deviceID) };
+        }
+    }
+}
---
+++ LiftOff.API/Logic/SmartwatchPairer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiftOff.API.Logic
+{
+    public class SmartwatchPairer
+    {
+        public static readonly SmartwatchPairer Instance = new SmartwatchPairer();
+
+        public SmartwatchPairer()
+        {
+            initiateListCleaner();
+        }
+
+        public static List<MobileDevice> RegisteredDevices = new List<MobileDevice>();
+
+        public void RegisterMobileDevice(string deviceID, string token)
+        {
+            object lockObj = new object();
+            lock (lockObj)
+            { 
+                RegisteredDevices.Add(new MobileDevice()
+                {
+                    DeviceID = deviceID,
+                    Token = token,
+                    TimeRegistered = DateTime.Now
+                });
+            }
+        }
+
+        public string GetRegisteredMobileDeviceToken(string deviceID)
+        {
+            return RegisteredDevices.FirstOrDefault(d => d.DeviceID == deviceID).Token;
+        }
+
+        public void initiateListCleaner()
+        {
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromHours(1);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                RegisteredDevices.RemoveAll(x => DateTime.Now - x.TimeRegistered > TimeSpan.FromDays(1));
+            }, null, startTimeSpan, periodTimeSpan);
+        }
+    }
+
+    public struct MobileDevice
+    {
+        public string DeviceID { get; set; }
+        public string Token { get; set; }
+        public DateTime TimeRegistered { get; set; }
+    }
+}
