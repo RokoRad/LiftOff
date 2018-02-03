3a150df198b0abfef58b0c900eb86affbcad86c3
dbaric <drazen@dump.hr>
2018-02-03 18:59:47 +0100

    :nail_care:

---
+++ LiftOff.Mobile/components/InitialBackground/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import vars from '../../config/vars.js';
+
+export default StyleSheet.create({
+  image: {
+    position: 'aboslute',
+    top: 0,
+    left: 0,
+    right: 0, 
+    bottom: 0
+  }
+});
