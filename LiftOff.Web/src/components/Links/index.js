573a400688f6de408032520986674effe08c115e
dbaric <drazen@dump.hr>
2018-02-04 12:03:06 +0100

    :nail_care:

--- LiftOff.Web/src/components/Links/index.js
+++ LiftOff.Web/src/components/Links/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Link from '../Link';
 import './style.css';
 
-export default ({}) => (
+export default ({ }) => (
   <div className="links">
-    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
-    <Link type="pdf" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
-    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
-    <Link type="pdf" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
-    <Link type="link" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
-    <Link type="pdf" link="www.google.com" title="Pravilnik o nečemu unutar zabranjenih zona" />
+    <Link type="link" link="www.google.com" title="Pravilnik o specifikacijama drona" />
+    <Link type="pdf" link="www.google.com" title="Pravilnik upravljanju dronom" />
+    <Link type="link" link="www.google.com" title="Pravne kategorije bespilotnih letjelica" />
+    <Link type="pdf" link="www.google.com" title="Pravilnik o zrabanjenim zonama leta" />
+    <Link type="link" link="www.google.com" title="Zakon o očuvanju okoliša" />
+    <Link type="pdf" link="www.google.com" title="Postupak registriranja drona" />
   </div>
 );
